43ec168b9fd25bdcc2a11b672278870992864a12
François Gaits <63-fgaits@users.noreply.022e47118ec0>
2021-06-08 13:55:05 +0000

    streaming real time rbg frames from lidar camera

--- src/imStream/ImageAcquisition.h
+++ src/imStream/ImageAcquisition.h
@@ -1,6 +1,13 @@
 #ifndef HUBV2_IMAGEACQUISITION_H
 #define HUBV2_IMAGEACQUISITION_H
 
+#include <vector>
+
+enum DataType {
+    RAW=0,
+    COMPRESSED
+};
+
 class ImageAcquisition {
 public:
     virtual std::vector<unsigned char> image() = 0;
@@ -10,6 +17,13 @@ public:
 
     virtual bool start() = 0;
     virtual void stop() = 0;
+
+    inline int dataType() const {
+        return _dataType;
+    }
+
+protected:
+    DataType _dataType = COMPRESSED;
 
 };
 
