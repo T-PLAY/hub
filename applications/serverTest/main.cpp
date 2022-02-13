57bf2b8d2022572abb8dbab3c67ea89b92a2490e
gauthier <gauthierbouyjou@aol.com>
2022-02-13 18:35:49 +0100

    fix linux interrupt + win socklen

--- applications/serverTest/main.cpp
+++ applications/serverTest/main.cpp
@@ -2,44 +2,62 @@
 
 #if defined WIN32
 #include <winsock.h>
-# pragma comment(lib,"ws2_32.lib") //Winsock Library
+#pragma comment(lib, "ws2_32.lib") // Winsock Library
+using socklen_t = int;
 
 #else
 #define closesocket close
-#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <csignal>
+#include <sys/socket.h>
 #include <unistd.h>
 #endif
 
 #define PORT 4043
 
-#include <stdio.h>
+#include <cstring>
 #include <iostream>
+#include <stdio.h>
 #include <thread>
 
-void ClearWinSock() {
+void ClearWinSock()
+{
 #if defined WIN32
     WSACleanup();
 #endif
 }
 
-void ErrorHandler(char *errorMessage) {
+void ErrorHandler(char* errorMessage)
+{
     printf(errorMessage);
 }
 
+int server_fd;
+void signalHandler(int signum)
+{
+    std::cout << "Interrupt signal (" << signum << ") received." << std::endl;
 
-int main(void) {
+    // cleanup and close up stuff here
+    // terminate program
+    closesocket(server_fd);
+
+    exit(signum);
+}
+
+int main(void)
+{
 #if defined WIN32
     WSADATA wsaData;
-    int iResult = WSAStartup(MAKEWORD(2 ,2), &wsaData);
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (iResult != 0) {
         printf("error at WSASturtup\n");
         return 0;
     }
+#else
+    signal(SIGINT, signalHandler);
 #endif
 
     // Socket creation
-    int server_fd;
     server_fd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (server_fd < 0) {
         ErrorHandler("socket creation failed.\n");
@@ -50,7 +68,7 @@ int main(void) {
 
     // Server address construction
     struct sockaddr_in address;
-    int addrlen = sizeof(address);
+    socklen_t addrlen = sizeof(address);
     memset(&address, 0, sizeof(address));
     address.sin_family = AF_INET;
     //    address.sin_addr.s_addr = inet_addr("192.168.0.100"); // server IP
@@ -58,7 +76,7 @@ int main(void) {
     address.sin_port = htons(PORT); // Server port
 
     // Create server
-    if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
         ErrorHandler("Failed to bind.\n");
         closesocket(server_fd);
         ClearWinSock();
@@ -73,7 +91,6 @@ int main(void) {
         return 0;
     }
 
-
     constexpr size_t imgSize = 192 * 512;
     unsigned char img[imgSize];
     int new_socket;
@@ -81,7 +98,7 @@ int main(void) {
 
         std::cout << "wait client on port " << PORT << std::endl;
         if ((new_socket = accept(server_fd, (struct sockaddr*)&address,
-                                 &addrlen))
+                 &addrlen))
             < 0) {
             perror("accept");
             //            exit(EXIT_FAILURE);
@@ -100,12 +117,12 @@ int main(void) {
 
             int byteRead = recv(new_socket, (char*)&a, 1, 0);
             if (byteRead != 1) {
-                std::cout << "can't read sync byte " << byteRead  << std::endl;
-//                std::this_thread::sleep_for(std::chrono::milliseconds(100));
-//                byteRead = recv(new_socket, (char*)&a, 1, 0);
-//                closesocket(server_fd);
-//                ClearWinSock();
-//                return 4;
+                std::cout << "can't read sync byte " << byteRead << std::endl;
+                //                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                //                byteRead = recv(new_socket, (char*)&a, 1, 0);
+                //                closesocket(server_fd);
+                //                ClearWinSock();
+                //                return 4;
                 break;
             }
             std::cout << "read a : " << (int)a << std::endl;
@@ -120,12 +137,12 @@ int main(void) {
 
             int byteSent = send(new_socket, (char*)img, imgSize, 0);
             if (byteSent != imgSize) {
-                std::cout << "can't send sync byte " << byteSent  << std::endl;
-//                std::this_thread::sleep_for(std::chrono::milliseconds(100));
-//                byteSent = send(new_socket, (char*)img, imgSize, 0);
-//                closesocket(server_fd);
-//                ClearWinSock();
-//                return 3;
+                std::cout << "can't send sync byte " << byteSent << std::endl;
+                //                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                //                byteSent = send(new_socket, (char*)img, imgSize, 0);
+                //                closesocket(server_fd);
+                //                ClearWinSock();
+                //                return 3;
                 break;
             }
 
