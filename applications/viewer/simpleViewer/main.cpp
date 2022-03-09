464de8c9b35448ee751e73fd72baf8f76a60cdd4
Gauthier Bouyjou <Gauthier.Bouyjou@irit.fr>
2022-03-09 13:42:30 +0200

    added Polhemus streamer

--- applications/viewer/simpleViewer/main.cpp
+++ applications/viewer/simpleViewer/main.cpp
@@ -10,23 +10,29 @@ int main()
 
     while (true) {
         try {
-            InputStream inputStream("proceduralStreamer");
+            // InputStream inputStream("proceduralStreamer");
             // InputStream inputStream("L500 Depth Sensor (Depth)");
+            InputStream inputStream("Polhemus Patriot (probe)");
 
             const size_t acquisitionSize = inputStream.getAcquisitionSize();
-            const int width = inputStream.getDims().at(0);
+            std::cout << "acquisitionSize = " << acquisitionSize << std::endl;
+            // const int width = inputStream.getDims().at(0);
 
             Stream::Acquisition acq;
 
             while (true) {
                 inputStream >> acq;
 
-                const int dec = acq.mData[0];
-                for (size_t i = 0; i < acquisitionSize; ++i) {
-                    assert(acq.mData[i] == (i / width + dec) % 256);
-                }
+                // const int dec = acq.mData[0];
+                // for (size_t i = 0; i < acquisitionSize; ++i) {
+                //     assert(acq.mData[i] == (i / width + dec) % 256);
+                // }
 
                 std::cout << "[simpleViewer] receive acquisition : " << acq << std::endl;
+//                float* data = (float*)acq.mData;
+                float* translation = (float*)acq.mData;
+                float* quaternion = (float*)&acq.mData[12];
+                std::cout << "x:" << translation[0] << ", y:" << translation[1] << ", z:" << translation[2] << ", az:" << quaternion[0] << ", el:" << quaternion[1] << ", ro:" << quaternion[2] << ", q4:" << quaternion[3] << std::endl;
             }
 
         } catch (const std::exception& e) {
