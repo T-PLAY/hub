d40ae00d8274ae3af94150ff4411ff32e1d910dc
gauthier <gauthierbouyjou@aol.com>
2022-04-08 16:28:10 +0300

    added metadata
    Former-commit-id: 25252c80312c83b4403e95dcec8693a13bf96831 [formerly 45c281fe284b8442e3c9ba56553c1ef1803c198e]
    Former-commit-id: 3a3034e674bed7377e0279795f9557d91803f5dd

--- src/IOStream.h
+++ src/IOStream.h
@@ -1,22 +1,24 @@
 #ifndef IOSTREAM_H
 #define IOSTREAM_H
 
+#include <any>
 #include <cassert>
 #include <iostream>
 #include <list>
+#include <map>
 #include <string>
-#include <vector>
 #include <typeinfo>
+#include <vector>
 
 //#define DEBUG_IOSTREAM
 
 class IOStream {
 public:
     IOStream() = default;
-    IOStream(IOStream && ioStream) = default;
-//    IOStream(const IOStream & ioStream) = delete;
-    IOStream(IOStream & ioStream) = delete;
-//    IOStream(IOStream ioStream) = delete;
+    IOStream(IOStream&& ioStream) = default;
+    //    IOStream(const IOStream & ioStream) = delete;
+    IOStream(IOStream& ioStream) = delete;
+    //    IOStream(IOStream ioStream) = delete;
     IOStream& operator=(const IOStream& ioStream) = delete;
     IOStream&& operator=(const IOStream&& ioStream) = delete;
     IOStream& operator=(IOStream& ioStream) = delete;
@@ -24,7 +26,12 @@ public:
 
     virtual ~IOStream() = default;
 
+//    template <class T>
+//    static const auto getValue(const std::any& any);
+
+    void write(const std::any& any) const;
     virtual void write(const unsigned char* data, size_t len) const = 0;
+    void write(const char* str) const;
 
     template <class T>
     void write(const T& t) const;
@@ -32,10 +39,16 @@ public:
     void write(const std::list<T>& list) const;
     template <class T>
     void write(const std::vector<T>& vector) const;
+    template <class T, class U>
+    void write(const std::map<T, U>& map) const;
 
     void write(const std::string& str) const;
 
+    ////////////////////////////////////////////////////////////////////////////
+
+    void read(std::any& any) const;
     virtual void read(unsigned char* data, size_t len) const = 0;
+    void read(char * str) const;
 
     template <class T>
     void read(T& t) const;
@@ -43,6 +56,8 @@ public:
     void read(std::list<T>& list) const;
     template <class T>
     void read(std::vector<T>& vector) const;
+    template <class T, class U>
+    void read(std::map<T, U>& map) const;
 
     void read(std::string& str) const;
 
@@ -53,7 +68,7 @@ public:
         INPUT_OUTPUT,
     };
 
-//    virtual IOStream(IOStream && iostream) = default;
+    //    virtual IOStream(IOStream && iostream) = default;
 
     //    IOStream(const Mode& mode);
     //    virtual void setupMode(const Mode& mode) = 0;
@@ -65,6 +80,32 @@ private:
     //    Mode mMode;
 };
 
+//template <class T>
+//auto IOStream::getValue(const std::any& any) -> decltype (int)
+//{
+//    assert(any.has_value());
+//    const auto & type = any.type();
+////    type_info;
+
+//    //    write(hashCode);
+//    if (type == "double") {
+//        const double* val = std::any_cast<double>(&any);
+//        return val;
+//        //        write(*val);
+
+//    } else if (type == "class std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> >") {
+//        const std::string* val = std::any_cast<std::string>(&any);
+//        //        write(*val);
+//        return val;
+
+//    } else {
+//        auto name = any.type().name();
+//        auto raw_name = any.type().raw_name();
+//        assert(false);
+//    }
+//    return nullptr;
+//}
+
 template <class T>
 void IOStream::write(const T& t) const
 {
@@ -75,7 +116,6 @@ void IOStream::write(const T& t) const
     //    assert(mMode != Mode::INPUT || std::is_enum<T>::value);
     assert(mMode == Mode::OUTPUT || mMode == Mode::INPUT_OUTPUT);
     write(reinterpret_cast<const unsigned char*>(&t), sizeof(T));
-
 }
 
 template <class T>
@@ -97,6 +137,21 @@ void IOStream::write(const std::vector<T>& vector) const
 
     for (const T& el : vector) {
         write(el);
+    }
+}
+
+template <class T, class U>
+void IOStream::write(const std::map<T, U>& map) const
+{
+    int nbKey = static_cast<int>(map.size());
+    write(nbKey);
+
+    for (const auto& pair : map) {
+        const T& first = pair.first;
+        const U& second = pair.second;
+
+        write(first);
+        write(second);
     }
 }
 
@@ -138,6 +193,23 @@ void IOStream::read(std::vector<T>& vector) const
         T el;
         read(el);
         vector.push_back(std::move(el));
+    }
+}
+
+//template <class T = std::string, class U = std::any>
+template <class T, class U>
+void IOStream::read(std::map<T, U>& map) const
+{
+    int nbEl;
+    read(nbEl);
+
+    for (int i = 0; i < nbEl; ++i) {
+        T name;
+        read(name);
+        U val;
+        read(val);
+        assert(map.find(name) == map.end());
+        map[name] = val;
     }
 }
 
