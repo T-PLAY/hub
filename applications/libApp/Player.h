aa67bda26a95390e56d8a6097b1d2b06a1ad572f
gauthier <gauthierbouyjou@aol.com>
2022-05-15 19:06:17 +0200

    play record done with frame view
    Former-commit-id: 7680839bc5b9ccbab2dbfc59850b0cd9220ed00a [formerly 4b7e713b5bf10fddbd561e512542cd4d9f3a22e0]
    Former-commit-id: 2c7ea49f060594410e6d42c66cebbd87c8a54007

--- applications/libApp/Player.h
+++ applications/libApp/Player.h
@@ -5,6 +5,7 @@
 #include <stream.h>
 #include <future>
 
+
 struct Snapshot {
     Stream::Acquisition mAcq;
     std::string mSensorName;
@@ -13,6 +14,15 @@ struct Snapshot {
     {
         return mAcq.mBackendTimestamp < snapshot.mAcq.mBackendTimestamp;
     }
+    bool operator==(const Snapshot& snapshot) const
+    {
+        return mSensorName == snapshot.mSensorName && mAcq.mBackendTimestamp == snapshot.mAcq.mBackendTimestamp;
+    }
+};
+
+struct Frame {
+//    std::vector<Snapshot> m_snapshots;
+    std::vector<const Snapshot*> m_snapshots;
 };
 
 
@@ -28,11 +38,15 @@ public:
     void play();
     void stop();
 
+    void showFrame(int iFrame);
+
+
 
 private:
 
     std::map<std::string, std::unique_ptr<OutputStream>> m_outputs;
     std::set<Snapshot> m_snapshots;
+    std::vector<Frame> m_frames;
     std::string m_loadedPath = "";
 
 //    std::promise<void> m_exitSignal;
@@ -40,9 +54,14 @@ private:
     std::thread * m_thread = nullptr;
     bool m_isPlaying = false;
 //    bool m_exitSignal = false;
+    int m_currentFrame = -1;
 
 public:
+    const std::vector<Frame> & getFrames() const;
+    bool isPlaying() const;
+    bool isLoaded() const;
     const std::string & getLoadedPath() const;
+    int getCurrentFrame() const;
 };
 
 #endif // PLAYER_H
