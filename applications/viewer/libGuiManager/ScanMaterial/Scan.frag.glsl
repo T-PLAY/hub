6f5be5830130d4fe94d339e79d13a6e3331a57a9
gauthier <gauthierbouyjou@aol.com>
2022-08-27 14:07:41 +0200

    commit before break stream viewer
    Former-commit-id: b9d2a2ecf2bafa59ff789d5f6bc52939105ef3d5 [formerly f17c4aaef05383b578312b89c6290a619975e4a3]
    Former-commit-id: 369caeafa73bc78a4a501b3e02757817e123d3aa

--- applications/viewer/libGuiManager/ScanMaterial/Scan.frag.glsl
+++ applications/viewer/libGuiManager/ScanMaterial/Scan.frag.glsl
@@ -1,5 +1,5 @@
-#include "DefaultLight.glsl"
 #include "Scan.glsl"
+#include "DefaultLight.glsl"
 #include "VertexAttribInterface.frag.glsl"
 
 out vec4 out_color;
@@ -9,4 +9,12 @@ void main() {
     if ( toDiscard( material, bc ) ) discard;
     //out_color = vec4( bc.rgb, 1 );
     out_color = vec4( bc.rrr, 1 );
+
+//    float color = (bc.b + bc.g * 255.0) / 255.0;
+    float color = bc.g;
+//    color = 0.9;
+//    float alpha = 1.0 - color;
+    float alpha = color;
+    if (alpha < 0.1) discard;
+    out_color = vec4(vec3(color), 1.0);
 }
