1cd0f2585b8c8685238899a16ea08f292a550111
gauthier <gauthierbouyjou@aol.com>
2022-11-01 15:35:57 +0100

    format cmake files
    Former-commit-id: 28660b7e4662eef37f206d9303d869cb9788e31f [formerly 73cb9460963292bccd51b91f14819e0e2df6677f]
    Former-commit-id: effd2435117966a45162c66231b2a3d8025c5c4d

--- cmake/Findpdi.cmake
+++ cmake/Findpdi.cmake
@@ -1,31 +1,28 @@
+# set(PDI_SDK_DIR "") find_path(PDI_SDK_DIR Inc PATHS "C:/Polhemus/PDI" DOC "PDI include path"
+# #PATH_SUFFIXES Inc ) message(STATUS "hello ${PDI_SDK_DIR}")
 
-#set(PDI_SDK_DIR "")
-#find_path(PDI_SDK_DIR
-#    Inc
-#    PATHS "C:/Polhemus/PDI"
-#    DOC "PDI include path"
-#    #PATH_SUFFIXES Inc
-#    )
-#    message(STATUS "hello ${PDI_SDK_DIR}")
-
-
-set(PDI_SDK "<UNSET>" CACHE PATH "Path of PDI SDK dir on your system")
-if (WIN32 AND EXISTS "C:/Polhemus/PDI/PDI_140")
+set(PDI_SDK
+    "<UNSET>"
+    CACHE PATH "Path of PDI SDK dir on your system")
+if(WIN32 AND EXISTS "C:/Polhemus/PDI/PDI_140")
     set(PDI_SDK "C:/Polhemus/PDI/PDI_140")
 endif()
-if (PDI_SDK STREQUAL "<UNSET>")
-    message(FATAL_ERROR "You must set PDI_SDK path of PDI_140 (WIN) or piterm-1.0.4 (LINUX) folder in your system see : https://ftp.polhemus1.com/pub/Trackers/Patriot/Software/ https://ftp.polhemus1.com/pub/Software/PDI_4.4.1_Setup-SWD-PS08-08.exe or https://ftp.polhemus1.com/pub/Software/Linux/")
+if(PDI_SDK STREQUAL "<UNSET>")
+    message(
+        FATAL_ERROR
+            "You must set PDI_SDK path of PDI_140 (WIN) or piterm-1.0.4 (LINUX) folder in your system see : https://ftp.polhemus1.com/pub/Trackers/Patriot/Software/ https://ftp.polhemus1.com/pub/Software/PDI_4.4.1_Setup-SWD-PS08-08.exe or https://ftp.polhemus1.com/pub/Software/Linux/"
+    )
 endif()
 
-if (WIN32)
+if(WIN32)
     file(GLOB PDI_DLL ${PDI_SDK}/Lib/x64/P*.dll)
-#    file(GLOB PDI_DLL ${PDI_SDK}/Lib/x64/PDI.dll)
+    # file(GLOB PDI_DLL ${PDI_SDK}/Lib/x64/PDI.dll)
 
-    find_library(PDI_LIBRARY
+    find_library(
+        PDI_LIBRARY
         NAMES pdi
         PATHS ${PDI_SDK}/Lib/x64
-        DOC "PDI library"
-        )
+        DOC "PDI library")
     add_library(pdi INTERFACE IMPORTED)
     set(PDI_INCLUDE_DIR ${PDI_SDK}/Inc)
 
@@ -40,25 +37,14 @@ else()
     set(PDI_INCLUDE_DIR ${PDI_SDK}/src/)
 endif()
 
-
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(pdi
-    REQUIRED_VARS
-    PDI_LIBRARY
-    PDI_INCLUDE_DIR
-    )
+find_package_handle_standard_args(pdi REQUIRED_VARS PDI_LIBRARY PDI_INCLUDE_DIR)
 
-mark_as_advanced(PDI_FOUND
-    PDI_LIBRARY
-    PDI_INCLUDE_DIR
-    )
+mark_as_advanced(PDI_FOUND PDI_LIBRARY PDI_INCLUDE_DIR)
 
-if (PDI_FOUND)
-        set_target_properties(pdi PROPERTIES
-            INTERFACE_LINK_LIBRARIES ${PDI_LIBRARY}
-            INTERFACE_INCLUDE_DIRECTORIES ${PDI_INCLUDE_DIR}
-        )
+if(PDI_FOUND)
+    set_target_properties(pdi PROPERTIES INTERFACE_LINK_LIBRARIES ${PDI_LIBRARY}
+                                         INTERFACE_INCLUDE_DIRECTORIES ${PDI_INCLUDE_DIR})
 else()
     message(FATAL_ERROR "Could not find PDI.")
 endif()
-
