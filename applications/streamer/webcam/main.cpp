3bb463b020a607d46a2e87cd226103cbafe37a7a
gauthier <gauthierbouyjou@aol.com>
2022-11-15 14:24:02 +0100

    update opencv application
    Former-commit-id: ca885e32aaf13e69ee22e889631bd759b73dcd4f [formerly 92c08a2c67a57d62547611f285f2150264d2b0f8]
    Former-commit-id: c6fdb0c7dbaa4631fd18a20973e102c70fbffe66

--- applications/streamer/webcam/main.cpp
+++ applications/streamer/webcam/main.cpp
@@ -10,62 +10,72 @@
 #include <IO/Stream.hpp>
 #include <OutputSensor.hpp>
 
-int main(int, char**)
-{
+int main( int, char** ) {
     cv::Mat frame;
     //--- INITIALIZE VIDEOCAPTURE
     cv::VideoCapture cap;
     // open the default camera using default API
     // cap.open(0);
     // OR advance usage: select any API backend
-    int deviceID = 0; // 0 = open default camera
-    int apiID = cv::CAP_ANY; // 0 = autodetect default API
+    int deviceID = 0;           // 0 = open default camera
+    int apiID    = cv::CAP_ANY; // 0 = autodetect default API
     // open selected camera using selected API
-    cap.open(deviceID, apiID);
+    cap.open( deviceID, apiID );
     // check if we succeeded
-    if (!cap.isOpened()) {
+    if ( !cap.isOpened() ) {
         std::cerr << "ERROR! Unable to open camera\n";
         return -1;
     }
-        std::cout << "Able to open camera\n";
+    std::cout << "Able to open camera\n";
 
-    const int width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
-    const int height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+    const int width  = cap.get( cv::CAP_PROP_FRAME_WIDTH );
+    const int height = cap.get( cv::CAP_PROP_FRAME_HEIGHT );
     //    OutputStream outputStream( "webcam", Stream::Format::BGR8, { width, height } );
-    hub::SensorSpec sensorSpec("Webcam", { { { width, height }, hub::SensorSpec::Format::BGR8 } });
-    hub::OutputSensor outputSensor(sensorSpec, hub::io::OutputStream("Webcam"));
+    hub::SensorSpec sensorSpec( "Webcam",
+                                { { { width, height }, hub::SensorSpec::Format::BGR8 } } );
+    hub::OutputSensor outputSensor( sensorSpec, hub::io::OutputStream( "Webcam" ) );
 
     //--- GRAB AND WRITE LOOP
-    std::cout << "Start grabbing" << std::endl
-              << "Press any key to terminate" << std::endl;
-    for (;;) {
-        // wait for a new frame from camera and store it into 'frame'
-        auto start = std::chrono::duration_cast<std::chrono::microseconds>(
-            std::chrono::system_clock::now().time_since_epoch())
-                         .count();
-        cap >> frame;
-//        std::cout << "New frame" << std::endl;
+    std::cout << "Start grabbing" << std::endl << "Press any key to terminate" << std::endl;
+    bool stopped       = false;
+    std::thread thread = std::thread( [&]() {
+        while ( !stopped ) {
+            // wait for a new frame from camera and store it into 'frame'
+            auto start = std::chrono::duration_cast<std::chrono::microseconds>(
+                             std::chrono::system_clock::now().time_since_epoch() )
+                             .count();
+            cap >> frame;
+            //        std::cout << "New frame" << std::endl;
 
-        auto end = std::chrono::duration_cast<std::chrono::microseconds>(
-            std::chrono::system_clock::now().time_since_epoch())
-                       .count();
+            auto end = std::chrono::duration_cast<std::chrono::microseconds>(
+                           std::chrono::system_clock::now().time_since_epoch() )
+                           .count();
 
-        cv::Mat grey;
-        cv::cvtColor(frame, grey, cv::COLOR_BGR2GRAY);
-        //        outputStream << Stream::Acquisition( start, end, frame.data, 3 * width * height );
-        outputSensor << (hub::Acquisition { start, end } << hub::Measure { frame.data, (uint64_t)(3 * width * height) });
+            cv::Mat grey;
+            cv::cvtColor( frame, grey, cv::COLOR_BGR2GRAY );
+            //        outputStream << Stream::Acquisition( start, end, frame.data, 3 * width *
+            //        height );
+            outputSensor << ( hub::Acquisition { start, end }
+                              << hub::Measure { (const unsigned char* const)frame.data,
+                                                (uint64_t)( 3 * width * height ) } );
 
-        // check if we succeeded
-        if (frame.empty()) {
-            std::cerr << "ERROR! blank frame grabbed\n";
-            break;
+            // check if we succeeded
+            if ( frame.empty() ) {
+                std::cerr << "ERROR! blank frame grabbed\n";
+                break;
+            }
+            // show live and wait for a key with timeout long enough to show images
+            // esc
+            //        if ((char)27 == cv::waitKey(10)) break;
+            //        if (cv::waitKey(20) > 0) break;
+            //        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         }
-        // show live and wait for a key with timeout long enough to show images
-        // esc
-//        if ((char)27 == cv::waitKey(1)) break;
-        if (cv::waitKey(10) > 0) break;
-//        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    }
+    } );
+
+    getchar();
+    stopped = true;
+    thread.join();
+
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
 }
