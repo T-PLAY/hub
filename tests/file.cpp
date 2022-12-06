c67d69098322ebebc79f09a878668e41bac05f08
Gauthier Bouyjou <Gauthier.Bouyjou@irit.fr>
2022-12-06 17:45:16 +0100

    added test to CI

--- tests/file.cpp
+++ tests/file.cpp
@@ -47,7 +47,7 @@ TEST_CASE( "File test" ) {
     }
     std::cout << "outputStream end ################################" << std::endl;
 
-    std::this_thread::sleep_for( std::chrono::milliseconds( 1000 ) );
+//    std::this_thread::sleep_for( std::chrono::milliseconds( 1000 ) );
 
     std::cout << "inputStream start" << std::endl;
     INFO( "InputStream" );
