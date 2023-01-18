4f82561b21c0dc2391b9dab7ef51fcfd530ab251
Gauthier Bouyjou <Gauthier.Bouyjou@irit.fr>
2023-01-18 22:26:00 +0100

    update cleanup scripts

--- scripts/source-format-all.sh
+++ scripts/source-format-all.sh
@@ -7,7 +7,7 @@ done
 
 
 cd $rootPath
-for file in $(find src tests server examples doc -type f \( -name "*.cpp" -o -name "*.hpp" -o -name "*.h" -o -name "*.inl" \)); do
+for file in $(find $(cat scripts/sources.txt) -type f \( -name "*.cpp" -o -name "*.hpp" -o -name "*.h" -o -name "*.inl" \)); do
 	echo $file
 	clang-format -i --style=file $file
 done
