302da43b24a8057f5213c1740e2b9c58478c4dd4
gauthier <gauthierbouyjou@aol.com>
2023-01-18 14:59:39 +0100

    update cmake doc
    Former-commit-id: 25989993601e7a879c2bdb9c6375a956771d7e2c [formerly b35e8633050808baa6671c69bb2b045e0cf400c5]
    Former-commit-id: c49317b4bc73f52743a8d27712b1772c13cd4624

--- doc/include/cpp_doxygen_sphinx.hpp
+++ doc/include/cpp_doxygen_sphinx.hpp
@@ -2,11 +2,9 @@
 #include <iostream>
 #include <string>
 
-/// Foo
 class Foo {
+public:
 
-    /// Say hello
-    /// @param message The message to print
     void say_hello(std::string message) const;
 
 };
