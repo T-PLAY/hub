e8f8ecbba6d5183c559fd5c4277218379cf1e65f
gauthier <gauthierbouyjou@aol.com>
2023-01-18 18:49:45 +0100

    update cleanup scripts
    Former-commit-id: 37ab10b97ced071862360cab0d12927fa4ee3754 [formerly 088927ea347f751cef4924f2c8c7cf9ba9826e86]
    Former-commit-id: c9c21c5e5b763fccb87fd8976a0f95320e2214c1

--- scripts/cmake-format-all.sh
+++ scripts/cmake-format-all.sh
@@ -8,7 +8,7 @@ done
 
 cd $rootPath
 
-for file in $(find . -type f \( -name "CMakeLists.txt" -o -name "*.cmake" \)); do
+for file in $(find . -type f \( \( -name "CMakeLists.txt" -o -name "*.cmake" \) -not -path "*build*" -not -path "*install*" \)); do
 	echo $file
 	cmake-format -i $file
 done
--- scripts/source-format-all.sh
+++ scripts/source-format-all.sh
@@ -7,8 +7,7 @@ done
 
 
 cd $rootPath
-
-for file in $(find . -type f \( -name "*.cpp" -o -name "*.hpp" -o -name "*.h" -o -name "*.inl" \)); do
+for file in $(find . -type f \( -name "*.cpp" -o -name "*.hpp" -o -name "*.h" -o -name "*.inl" -not -path "*build*" -not -path "*install*" \)); do
 	echo $file
 	clang-format -i --style=file $file
 done
