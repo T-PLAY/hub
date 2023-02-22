af085247f734f1c2612423fb101e051172fc949e
Gauthier Bouyjou <Gauthier.Bouyjou@irit.fr>
2023-02-22 17:50:26 +0100

    added native for matlab

---
+++ src/Native2.cpp
@@ -0,0 +1,37 @@
+#include "Native2.hpp"
+
+/* Native.cpp : Defines the exported functions for the DLL.
+ */
+
+#include <iostream>
+
+//#include <cstring>
+//#include <sstream>
+
+//namespace hub {
+//namespace native2 {
+
+//__declspec(dllexport)
+    const char *getFuck( ) {
+//    printf("fuck\n");
+//    std::cout << "fuck" << std::endl;
+    return "fuck";
+}
+
+//void createInputSensor( const char* streamName, const char* ipv4, int port ) {
+//    std::cout << "[Native] createInputSensor( " << streamName << ")" << std::endl;
+//    InputSensor* inputSensor = nullptr;
+//    try {
+//        inputSensor =
+//            new InputSensor( io::InputStream( streamName, "", net::ClientSocket( ipv4, port ) ) );
+//    }
+//    catch ( std::exception& e ) {
+//        std::cout << "[Native] createInputSensor : catch exception : " << e.what() << std::endl;
+//        return nullptr;
+//    }
+//    return inputSensor;
+//}
+
+
+//} // namespace native2
+//} // namespace hub
