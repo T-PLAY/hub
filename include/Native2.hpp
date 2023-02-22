8fa3cacf34557bd1d3a1a4557662c8ed239f02dc
gauthier <gauthierbouyjou@aol.com>
2023-02-22 17:50:26 +0100

    added native for matlab
    Former-commit-id: b1e6703e7daa2675fefe6ec53374e754e712e70f [formerly 74a46030eea22ff37039c5f573389495e3d400df]
    Former-commit-id: ba15840d2bfc836283e517c0388a1c77d45961a1

---
+++ include/Native2.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Macros.hpp"
+
+//namespace hub {
+//namespace native2 {
+///
+
+//#ifndef NATIVE_EXPORT
+//#define NATIVE_EXPORT __declspec(dllimport)            // For Microsoft Visual Studio
+//#endif
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+//    NATIVE_EXPORT void getAcquisition( const char* streamName, const char* ipv4, int port );
+//    NATIVE_EXPORT void getAcquisition( );
+    SRC_API const char * getFuck( );
+
+
+#ifdef __cplusplus
+} // end extern "C"
+#endif
+
+//} // namespace native2
+//} // namespace hub
