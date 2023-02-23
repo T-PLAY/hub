45cde9b64d46cea9b36a63d2272c1c045ea8ffca
gauthier <gauthierbouyjou@aol.com>
2023-02-23 16:13:23 +0100

    added matlab api
    Former-commit-id: 1516b97cd863f842d0a6c4cb02dd9104b0ed3ce7 [formerly 4ac6e952400be081be6763710301ef5f2338fce1]
    Former-commit-id: 5eedc810523e11bdf0c9c6944e1fb0399e4e7117

---
+++ api/matlab/MexFunction.cpp
@@ -0,0 +1,12 @@
+
+#include "mex.hpp"
+#include "mexAdapter.hpp"
+
+class MexFunction : public matlab::mex::Function {
+public:
+    void operator()(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs) {
+        // check input arguments
+        // implement function
+    }
+};
+
