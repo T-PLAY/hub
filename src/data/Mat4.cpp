f0180c2f789d3a268f5de62731e443f01e535db0
gauthier <gauthierbouyjou@aol.com>
2023-03-14 22:11:16 +0100

    unignore data folder
    Former-commit-id: 78eefd460d09ce2e1019d900f7678cc8cda3e19a [formerly 6b4c853ebbc5b4b93f3b9c4116dd282a6ad10efc]
    Former-commit-id: b8fff4cf59b384a0b1341664e6de85b9f18aab28

---
+++ src/data/Mat4.cpp
@@ -0,0 +1,98 @@
+#include "Mat4.hpp"
+
+#include <iostream>
+
+
+namespace hub {
+namespace data {
+
+Mat4::Mat4( float* array ) :
+    Measure( reinterpret_cast<unsigned char*>( new float[16] ),
+             64,
+             Resolution { { 1 }, Format::MAT4 },
+             true ) {
+    memcpy( m_data, array, 64 );
+    assert( m_size == 64 );
+}
+
+Mat4::Mat4( float r0c0,
+            float r0c1,
+            float r0c2,
+            float r0c3,
+            float r1c0,
+            float r1c1,
+            float r1c2,
+            float r1c3,
+            float r2c0,
+            float r2c1,
+            float r2c2,
+            float r2c3,
+            float r3c0,
+            float r3c1,
+            float r3c2,
+            float r3c3 ) :
+    Measure( reinterpret_cast<unsigned char*>( new float[16] ),
+             64,
+             Resolution { { 1 }, Format::MAT4 },
+             true ) {
+
+    float params[16] { r0c0,
+                       r0c1,
+                       r0c2,
+                       r0c3,
+                       r1c0,
+                       r1c1,
+                       r1c2,
+                       r1c3,
+                       r2c0,
+                       r2c1,
+                       r2c2,
+                       r2c3,
+                       r3c0,
+                       r3c1,
+                       r3c2,
+                       r3c3 };
+    memcpy( m_data, params, 64 );
+    // row major
+
+    assert( m_size == 64 );
+}
+
+Mat4::Mat4( const Mat4& mat4 ) :
+    Measure( reinterpret_cast<unsigned char*>( new float[16] ),
+             64,
+             Resolution { { 1 }, Format::MAT4 },
+             true ) {
+    memcpy( m_data, mat4.m_data, 64 );
+}
+
+std::string Mat4::to_string() const {
+    float data[16];
+    memcpy( data, m_data, 64 );
+
+    std::string str = "[";
+    for ( int i = 0; i < 4; ++i ) {
+        for ( int j = 0; j < 4; ++j ) {
+            char buff[32];
+#ifdef WIN32
+            sprintf_s( buff, "%.0f ", val[4 * i + j] );
+#else
+            sprintf( buff, "%.0f", data[4 * i + j] );
+#endif
+            str += buff;
+            if ( j != 3 ) str += " ";
+        }
+        if ( i != 3 ) str += ", ";
+    }
+    (void)data;
+    return str + "]";
+}
+
+
+std::ostream& operator<<( std::ostream& os, const Mat4& mat4 ) {
+    os << mat4.getResolution() << ", [" << mat4.to_string() << "]";
+    return os;
+}
+
+} // namespace data
+} // namespace hub
---
+++ src/data/Measure.cpp
@@ -0,0 +1,88 @@
+#include "Measure.hpp"
+
+#include <iomanip>
+
+
+#include "data/Dof6.hpp"
+
+namespace hub {
+namespace data {
+
+Measure::~Measure() {
+
+    if ( m_ownData && !m_isMoved ) {
+        assert( m_data != nullptr );
+        delete[] m_data;
+    }
+}
+
+
+
+Measure::Measure( Measure&& measure ) :
+    m_data( measure.m_data ),
+    m_size( measure.m_size ),
+    m_ownData( measure.m_ownData ),
+    m_resolution( std::move( measure.m_resolution ) ) {
+
+    measure.m_isMoved = true;
+}
+
+
+
+
+
+
+Measure Measure::clone() const {
+    return { m_data, m_size, m_resolution };
+}
+
+bool Measure::isInterpolable() const {
+    assert( m_resolution.second != Format::NONE );
+    assert( !m_resolution.first.empty() );
+    return format2isInterpolable( m_resolution.second );
+}
+
+
+
+Measure Measure::slerp( const Measure& left, const Measure& right, double t ) {
+    assert( left.m_size == right.m_size );
+    assert( !left.m_resolution.first.empty() );
+    assert( !right.m_resolution.first.empty() );
+    assert( left.m_resolution == right.m_resolution );
+
+    if ( left.isInterpolable() ) {
+        assert( right.isInterpolable() );
+
+        switch ( left.m_resolution.second ) {
+        case Format::DOF6: {
+            return Dof6::slerp( Dof6( left ), Dof6( right ), t );
+        }
+        }
+    }
+    return left.clone(); // avoid non return warning
+}
+
+bool Measure::operator==( const Measure& measure ) const {
+    if ( m_size == measure.m_size ) {
+        return memcmp( m_data, measure.m_data, m_size ) == 0 &&
+               m_resolution == measure.m_resolution;
+    }
+    return false;
+}
+
+bool Measure::operator!=( const Measure& measure ) const {
+    return !( *this == measure );
+}
+
+std::ostream& operator<<( std::ostream& os, const Measure& measure ) {
+    os << measure.getResolution() << ", [";
+
+    for ( auto i = 0; i < std::min( (int)measure.m_size, 10 ); ++i ) {
+        os << std::setw( 3 ) << (int)measure.m_data[i] << " ";
+    }
+    os << "]";
+    return os;
+}
+
+} // namespace data
+} // namespace hub
