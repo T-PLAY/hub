f8828fbc09ded46a055b2dd6377d4d8ecdc91638
gauthier <gauthierbouyjou@aol.com>
2023-03-22 18:51:11 +0100

    fixup hub tests
    Former-commit-id: 2b1a6b3cf98807118adc53a2c0988b0ee8f3865e [formerly 5fff4941612e5a6cb82e3f10a566573b32a074b6]
    Former-commit-id: 18377bf474472a65510d5ff2b682f59de307225a

--- tests/server/test-serverSpeed.cpp
+++ tests/server/test-serverSpeed.cpp
@@ -1,4 +1,4 @@
-//#include <catch2/catch_test_macros.hpp>
+// #include <catch2/catch_test_macros.hpp>
 #include "test-common.hpp"
 
 #include <InputSensor.hpp>
@@ -10,35 +10,32 @@
 
 #define CHECK
 
-//TEST_CASE("Server test : speed test")
-int main()
-{
-    constexpr int nAcqs = 100;
-    constexpr int width = 1920;
-    constexpr int height = 1080;
+// TEST_CASE("Server test : speed test")
+int main() {
+    constexpr int nAcqs       = 200;
+    constexpr int width       = 3840;
+//    constexpr int width       = 1920;
+//    constexpr int width       = 640;
+    constexpr int height      = 2160;
+//    constexpr int height      = 1080;
+//    constexpr int height      = 480;
     constexpr size_t dataSize = width * height * 3;
+    unsigned char* data       = new unsigned char[dataSize];
+    unsigned char* data2      = new unsigned char[dataSize];
     //    unsigned char datas[nAcqs][dataSize];
-#ifdef WIN32
-    unsigned char** datas = new unsigned char*[nAcqs];
-    for (int i = 0; i < nAcqs; ++i) {
-        datas[i] = new unsigned char[dataSize];
-    }
-#else
-    unsigned char datas[nAcqs][dataSize];
-#endif
 
     //    return 0;
-    std::vector<hub::Acquisition> acqs(nAcqs);
+    std::vector<hub::Acquisition> acqs( nAcqs );
+    for ( int i = 0; i < dataSize; ++i ) {
+        data[i] = i % 256;
+    }
 
-    for (int iAcq = 0; iAcq < nAcqs; ++iAcq) {
-        for (int i = 0; i < dataSize; ++i) {
-            datas[iAcq][i] = iAcq;
-        }
-        hub::Acquisition acq(iAcq, iAcq);
-        acq << hub::data::Measure(reinterpret_cast<unsigned const char*>(datas[iAcq]),
-            dataSize,
-            { { width, height }, hub::Format::BGR8 });
-        acqs.at(iAcq) = std::move(acq);
+    for ( int iAcq = 0; iAcq < nAcqs; ++iAcq ) {
+        hub::Acquisition acq( iAcq, iAcq );
+        acq << hub::data::Measure( reinterpret_cast<unsigned const char*>( data ),
+                                   dataSize,
+                                   { { width, height }, hub::Format::BGR8 } );
+        acqs.at( iAcq ) = std::move( acq );
     }
     //    delete[] data;
 
@@ -47,14 +44,13 @@ int main()
     //    /////////////////////////////////////////////////
 
     const std::string ipv4 = "127.0.0.1";
-    const int port = GET_RANDOM_PORT;
+    const int port         = GET_RANDOM_PORT;
     //    std::vector<unsigned char> data(dataSize);
-#ifdef WIN32
-    unsigned char* data = new unsigned char[dataSize];
-#else
-    unsigned char data[dataSize];
-#endif
-    //    std::mutex mtx;
+    // #ifdef WIN32
+    // #else
+    //     unsigned char data[dataSize];
+    // #endif
+    //     std::mutex mtx;
 
     std::cout << "[test][ClientSocket] start streaming" << std::endl;
     const auto& start = std::chrono::high_resolution_clock::now();
@@ -73,10 +69,13 @@ int main()
         //                mtx.unlock();
         //            }
         //        });
-        hub::net::ServerSocket serverSocket(port);
-        hub::net::ClientSocket clientSocket(ipv4, port);
+        hub::net::ServerSocket serverSocket( port );
+        hub::net::ClientSocket clientSocket( ipv4, port );
         auto clientServerSocket = serverSocket.waitNewClient();
-        for (int iAcq = 0; iAcq < nAcqs; ++iAcq) {
+        const int packetSize = 2'000'000; // 2Go network memory buffer
+        const int nPart = dataSize / packetSize;
+        for ( int iAcq = 0; iAcq < nAcqs; ++iAcq ) {
+//            std::cout << "iAcq : " << iAcq << std::endl;
             //                mtx.lock();
             //                std::cout << "[ClientSocket] write data : ";
             //                for (int i = 0; i <dataSize; ++i) {
@@ -84,30 +83,48 @@ int main()
             //                }
             //                std::cout << std::endl;
             //                mtx.unlock();
-            clientSocket.write(datas[iAcq], dataSize);
-            clientServerSocket.read(data, dataSize);
-            assert(!memcmp(data, datas[iAcq], dataSize));
+//            const int len = dataSize;
+            int uploadSize = 0;
+
+            for ( int i = 0; i < nPart - 1; ++i ) {
+                clientSocket.write( data + uploadSize, packetSize );
+                clientServerSocket.read( data2 + uploadSize, packetSize );
+//                assert( !memcmp( data + uploadSize, data2 + uploadSize, packetSize ) );
+
+                uploadSize += packetSize;
+            }
+
+            clientSocket.write( data + uploadSize, dataSize - uploadSize );
+            clientServerSocket.read( data2 + uploadSize, dataSize - uploadSize );
+
+            assert( !memcmp( data, data2, dataSize ) );
+
         }
         //        threadServer.join();
     }
+    //    std::this_thread::sleep_for(std::chrono::duration())
+//    std::this_thread::sleep_for( std::chrono::milliseconds( 1 ) );
     const auto& end = std::chrono::high_resolution_clock::now();
     std::cout << "[test][ClientSocket] end streaming" << std::endl;
-    const auto& duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    const auto& bytes = dataSize * nAcqs;
-    const auto& bytesPerSeconds = 1000 * bytes / duration;
+    const auto& duration =
+        std::chrono::duration_cast<std::chrono::milliseconds>( end - start ).count();
+    const auto& bytes               = dataSize * nAcqs;
+    const auto& bytesPerSeconds     = 1000 * bytes / duration;
     const auto& megaBytesPerSeconds = bytesPerSeconds / 1000'000;
 
-    std::cout << "[test][ClientSocket] Mega byte wrote : " << bytes / 1000'000 << " Mo" << std::endl;
-    std::cout << "[test][ClientSocket] Mega byte per second : " << megaBytesPerSeconds << " Mo/s" << std::endl;
+    std::cout << "[test][ClientSocket] Mega byte wrote : " << bytes / 1000'000.0 << " Mo"
+              << std::endl;
+    std::cout << "[test][ClientSocket] Mega byte per second : " << megaBytesPerSeconds << " Mo/s"
+              << std::endl;
 
-#ifdef WIN32
-    delete[] data;
+    // #ifdef WIN32
+    //     delete[] data;
 
-    for (int i = 0; i < nAcqs; ++i) {
-        delete[] datas[i];
-    }
-    delete[] datas;
-#endif
+    //    for (int i = 0; i < nAcqs; ++i) {
+    //        delete[] datas[i];
+    //    }
+    //    delete[] datas;
+    // #endif
 
     //    return 0;
 
@@ -117,59 +134,81 @@ int main()
     {
         const int port2 = port + 1;
 
-        std::cout << "[test][InputOutputSensor] ############################### server start" << std::endl;
-        hub::server::Server server(port2);
-        server.setMaxClients(2);
+        std::cout << "[test][InputOutputSensor] ############################### server start"
+                  << std::endl;
+        hub::server::Server server( port2 );
+        server.setMaxClients( 2 );
         server.asyncRun();
-//        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        std::cout << "[test][InputOutputSensor] server end ------------------------------" << std::endl;
+        //        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::cout << "[test][InputOutputSensor] server end ------------------------------"
+                  << std::endl;
 
         {
-            std::cout << "[test][InputOutputSensor] ############################### outputStream start" << std::endl;
+            std::cout
+                << "[test][InputOutputSensor] ############################### outputStream start"
+                << std::endl;
             hub::OutputSensor outputSensor(
-                hub::SensorSpec("sensorName", { { { width, height }, hub::Format::BGR8 } }),
-                hub::io::OutputStream("streamName", hub::net::ClientSocket(ipv4, port2)));
+                hub::SensorSpec( "sensorName", { { { width, height }, hub::Format::BGR8 } } ),
+                hub::io::OutputStream( "streamName", hub::net::ClientSocket( ipv4, port2 ) ) );
 
             //            std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-            std::cout << "[test][InputOutputSensor] ############################### inputStream start" << std::endl;
+            std::cout
+                << "[test][InputOutputSensor] ############################### inputStream start"
+                << std::endl;
             hub::InputSensor inputSensor(
-                hub::io::InputStream("streamName", hub::net::ClientSocket(ipv4, port2)));
+                hub::io::InputStream( "streamName", hub::net::ClientSocket( ipv4, port2 ) ) );
 
             const auto& inputSensorSpec = inputSensor.getSpec();
-            CHECK(inputSensorSpec.getAcquisitionSize() == dataSize);
-            CHECK(inputSensorSpec.getSensorName() == "sensorName");
-            CHECK(inputSensorSpec.getResolutions().size() == 1);
-            CHECK(inputSensorSpec.getResolutions()[0].first.size() == 2);
-            CHECK(inputSensorSpec.getResolutions()[0].first.at(0) == width);
-            CHECK(inputSensorSpec.getResolutions()[0].first.at(1) == height);
-            CHECK(inputSensorSpec.getResolutions()[0].second == hub::Format::BGR8);
-            std::cout << "[test][InputOutputSensor] inputStream end ---------------------------------" << std::endl;
+            CHECK( inputSensorSpec.getAcquisitionSize() == dataSize );
+            CHECK( inputSensorSpec.getSensorName() == "sensorName" );
+            CHECK( inputSensorSpec.getResolutions().size() == 1 );
+            CHECK( inputSensorSpec.getResolutions()[0].first.size() == 2 );
+            CHECK( inputSensorSpec.getResolutions()[0].first.at( 0 ) == width );
+            CHECK( inputSensorSpec.getResolutions()[0].first.at( 1 ) == height );
+            CHECK( inputSensorSpec.getResolutions()[0].second == hub::Format::BGR8 );
+            std::cout
+                << "[test][InputOutputSensor] inputStream end ---------------------------------"
+                << std::endl;
 
-            std::cout << "[test][InputOutputSensor] ############################### send acquisitions" << std::endl;
+            std::cout
+                << "[test][InputOutputSensor] ############################### send acquisitions"
+                << std::endl;
             const auto& start = std::chrono::high_resolution_clock::now();
-            for (int i = 0; i < nAcqs; ++i) {
-                outputSensor << acqs.at(i);
+            for ( int i = 0; i < nAcqs; ++i ) {
+                outputSensor << acqs.at( i );
                 auto acq = inputSensor.getAcq();
-                CHECK(acq == acqs.at(i));
+                CHECK( acq == acqs.at( i ) );
             }
+//            std::this_thread::sleep_for( std::chrono::milliseconds( 1 ) );
             const auto& end = std::chrono::high_resolution_clock::now();
-            const auto& duration2 = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-            const auto& bytes2 = dataSize * nAcqs;
-            const auto& bytesPerSeconds2 = 1000 * bytes / duration2;
+            const auto& duration2 =
+                std::chrono::duration_cast<std::chrono::milliseconds>( end - start ).count();
+            const auto& bytes2               = dataSize * nAcqs;
+            const auto& bytesPerSeconds2     = 1000 * bytes / duration2;
             const auto& megaBytesPerSeconds2 = bytesPerSeconds2 / 1000'000;
 
-            std::cout << "[test][InputOutputSensor] Mega byte wrote : " << bytes2 / 1000'000 << " Mo" << std::endl;
-            std::cout << "[test][InputOutputSensor] Mega byte per second : " << megaBytesPerSeconds2 << " Mo/s" << std::endl;
+            std::cout << "[test][InputOutputSensor] Mega byte wrote : " << bytes2 / 1000'000.0
+                      << " Mo" << std::endl;
+            std::cout << "[test][InputOutputSensor] Mega byte per second : " << megaBytesPerSeconds2
+                      << " Mo/s" << std::endl;
 
             const auto ratio = 100.0 * megaBytesPerSeconds2 / megaBytesPerSeconds;
-            std::cout << "[test][ClientSocket/InputOutputSensor] ratio : " << ratio << " %" << std::endl;
-            CHECK (ratio > 45);
-//            CHECK(megaBytesPerSeconds >= 800); // server IRIT
+            std::cout << "[test][ClientSocket/InputOutputSensor] ratio : " << ratio << " %"
+                      << std::endl;
+#ifdef WIN32
+            CHECK( ratio > 45 );
+#else
+            CHECK( ratio > 50 );
+#endif
+            //            CHECK(megaBytesPerSeconds >= 800); // server IRIT
             // CHECK( megaBytesPerSeconds >= 2700 ); // home linux
         }
-//        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        //        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+
+    delete[] data;
+    delete[] data2;
 
     //    server.stop();
 }
