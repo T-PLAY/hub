68c7de4481730467f3a0f4d463eeb2b6d232edb3
gauthier <gauthierbouyjou@aol.com>
2023-03-29 18:36:33 +0200

    all (strong server) tests pass
    Former-commit-id: 4c2219dc7d87410be57dae6a12f46675f12012ae [formerly 6c5355e4720faee2544d44147ebfa381082c9498]
    Former-commit-id: 44ff981e1b40d17a4edb6a260cd8366b0d81b778

---
+++ tests/test-OutputSensorWithClosedServer.cpp
@@ -0,0 +1,55 @@
+#include "test-common.hpp"
+#include <catch2/catch_test_macros.hpp>
+
+#include <server/Server.hpp>
+
+TEST_CASE( "OutputSensor with closed server" ) {
+    const std::string ipv4 = "127.0.0.1";
+    const int port         = GET_RANDOM_PORT;
+
+    constexpr int delay = 0;
+
+    {
+
+        const hub::Resolution resolution( { { 1 }, hub::Format::BGR8 } );
+        const hub::SensorSpec sensorSpec( "sensorName", { resolution } );
+
+        unsigned char data[3] { 1, 2, 3 };
+        hub::Acquisition acq =
+            std::move( hub::Acquisition( 0, 1 ) << hub::data::Measure( data, 3, resolution ) );
+        std::vector<hub::Acquisition> acqs;
+        acqs.push_back(acq.clone());
+
+
+        hub::OutputSensor * outputSensor = nullptr;
+
+        std::this_thread::sleep_for( std::chrono::milliseconds( delay ) );
+        {
+
+            hub::server::Server server( port );
+            std::cout << "[Test] ############################### server start" << std::endl;
+            server.setMaxClients( 1 );
+            server.asyncRun();
+
+//            outputSensor = new hub::OutputSensor(sensorSpec, hub::io::OutputStream("streamName", hub::net::ClientSocket(ipv4, port)));
+            outputSensor = new hub::OutputSensor(sensorSpec, "streamName", hub::net::ClientSocket(ipv4, port));
+//            outputSensor = new hub::OutputSensor(hub::SensorSpec("sensorName", { resolution }), "streamName", hub::net::ClientSocket(ipv4, port));
+//            hub::OutputSensor outputSensor2(sensorSpec, "hello");
+
+            std::cout << "[Test] ############################### output sensor start" << std::endl;
+            std::this_thread::sleep_for( std::chrono::milliseconds( delay ) );
+            *outputSensor << acq;
+            std::cout << "[Test] ############################### sended acq" << std::endl;
+            std::this_thread::sleep_for( std::chrono::milliseconds( delay ) );
+
+//            server.detach();
+        }
+        std::cout << "[Test] ############################### server end" << std::endl;
+        std::this_thread::sleep_for( std::chrono::milliseconds( delay ) );
+
+        delete outputSensor;
+        std::cout << "[Test] ############################### output sensor end" << std::endl;
+
+    }
+    std::this_thread::sleep_for( std::chrono::milliseconds( delay ) );
+}
