06048dc3e8c0e5565f808d8521fb49c879dbd31f
gauthier <gauthierbouyjou@aol.com>
2023-03-30 23:50:34 +0200

    added lidar support
    Former-commit-id: 36188e120372babb0bd93bfb2bb59c795f2f3366 [formerly 8164cb793328d0d6fd19148e445f5e22118d2a83]
    Former-commit-id: 92fe14622bc180a720bcfc0b6b9261df12bd14d2

--- src/Resolution.hpp
+++ src/Resolution.hpp
@@ -50,7 +50,8 @@ enum class Format {
     Y411,     		/**< 12-bit per-pixel. */
     MAT4,     		/**< Transform matrix 4x4 of float. */
     USER_DATA,		/**< User data with name and any value */
-    MESH,			/**< Mesh */
+    MESH,			/**< Mesh consist of shapes of vertices with indexes */
+    POINT,			/**< Point consist of x, y and z position, rgb color and depth  */
     COUNT     		/**< Number of enumeration values. Not a valid input: intended to be used in for-loops. */
 };
 // clang-format on
@@ -156,6 +157,7 @@ static constexpr int s_format2nByte[static_cast<int>( Format::COUNT )] = {
     64,      // MAT4
     80 + 4,  // USER_DATA (non constant), string size ~= 80 and 4 bytes for common use int value
     80 + 4,  // MESH (non constant), string size ~= 80 and 4 bytes for common use int value
+    12 + 16,  // POINT
 };
 
 inline constexpr int format2nByte( const Format& format ) noexcept {
@@ -216,6 +218,7 @@ static constexpr bool s_format2isInterpolable[static_cast<int>( Format::COUNT )]
     false, // MAT4
     false, // USER_DATA
     false, // MESH
+    true, // POINT
 };
 
 inline constexpr bool format2isInterpolable( const Format& format ) noexcept {
@@ -239,7 +242,7 @@ static std::string format2stringArray[static_cast<int>( Format::COUNT )] = {
     "RAW16",       "RAW8",  "UYVY",        "MOTION_RAW", "MOTION_XYZ32F", "GPIO_RAW",
     "DISPARITY32", "6DOF",  "Y10BPACK",    "DISTANCE",   "MJPEG",         "Y8I",
     "Y12I",        "INZI",  "INVI",        "W10",        "Z16H",          "FG",
-    "Y411",        "MAT4",  "USER_DATA",   "MESH",
+    "Y411",        "MAT4",  "USER_DATA",   "MESH",		 "POINT",
 };
 
 static inline constexpr std::string& HUB_TO_STRING( const Format& format ) {
@@ -303,6 +306,7 @@ static constexpr bool s_format2hasFixedSize[static_cast<int>( Format::COUNT )] =
     true,  // MAT4
     false, // USER_DATA
     false, // MESH
+    true, // POINT
 };
 
 inline constexpr bool format2hasFixedSize( const Format& format ) noexcept {
