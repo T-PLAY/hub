3234bc70b29d483044f033137347f6bca0cf4cfa
Gauthier Bouyjou <Gauthier.Bouyjou@irit.fr>
2023-04-21 00:22:51 +0200

    tinyobj -> gltf

--- src/data/Mesh.cpp
+++ src/data/Mesh.cpp
@@ -1,20 +1,23 @@
-#define TINYOBJLOADER_IMPLEMENTATION
+// #define TINYOBJLOADER_IMPLEMENTATION
 #include "Mesh.hpp"
 
-//#include <filesystem>
+// #include <filesystem>
 #include <iostream>
 
 #include "io/Memory.hpp"
 
+#include <gltf/gltfpack.h>
+
 namespace hub {
 namespace data {
 
-Mesh::Mesh(const Measure &measure)
-    : Measure( measure.getData(), measure.getSize(), Resolution { { 1 }, Format::MESH } ) {
-//    assert( measure.getSize() == 64 );
-//    memcpy( (unsigned char*)&m_x, m_data, m_size );
+
+Mesh::Mesh( const Measure& measure ) :
+    Measure( measure.getData(), measure.getSize(), Resolution { { 1 }, Format::MESH } ) {
+    //    assert( measure.getSize() == 64 );
+    //    memcpy( (unsigned char*)&m_x, m_data, m_size );
     assert( m_data != nullptr );
-//    memcpy( m_data, measure.getData(), m_size );
+    //    memcpy( m_data, measure.getData(), m_size );
 
     unpack();
 }
@@ -23,117 +26,153 @@ Mesh::Mesh( const Mesh& mesh ) :
     Measure( new unsigned char[mesh.m_size],
              mesh.m_size,
              Resolution { { 1 }, Format::MESH },
-             true
-              ) {
+             true ) {
     memcpy( m_data, mesh.m_data, m_size );
-//    Measure( mesh.getData(), mesh.getSize(), Resolution { { 1 }, Format::MESH } ) {
-//    assert( m_data != nullptr );
+    //    Measure( mesh.getData(), mesh.getSize(), Resolution { { 1 }, Format::MESH } ) {
+    //    assert( m_data != nullptr );
     unpack();
 }
 
+Mesh::Mesh( const std::string& name ) :
+    Measure( (unsigned char*)nullptr, 0, Resolution { { 1 }, Format::MESH } ), m_name( name ) {}
+
+void Mesh::load( const std::string& fileObjPath ) {
+    //        std::filesystem::path path(fileObjPath);
+#ifdef WIN32
+    size_t slash = fileObjPath.find_last_of( "\\" );
+#else
+    size_t slash = fileObjPath.find_last_of( "/" );
+#endif
+    std::string dirPath =
+        ( slash != std::string::npos ) ? fileObjPath.substr( 0, slash ) : fileObjPath;
 
 
-Mesh::Mesh( const std::string& fileObjPath ) :
-    Measure( (unsigned char*)nullptr, 0, Resolution { { 1 }, Format::MESH } ) {
+    cgltf_data* data = 0;
+    std::vector<::Mesh> meshes;
+//    std::vector<Animation> animations;
 
-    //    std::filesystem::path path(fileObjPath);
-    // #ifdef WIN32
-    //    size_t slash = fileObjPath.find_last_of( "\\" );
-    // #else
-    //    size_t slash = fileObjPath.find_last_of( "/" );
-    // #endif
-    //    std::string dirPath = ( slash != std::string::npos ) ? fileObjPath.substr( 0, slash ) :
-    //    fileObjPath;
-    size_t lastindex = fileObjPath.find_last_of(".");
-    std::string filename = fileObjPath.substr(0, lastindex);
-    const std::string fileMtlPath = filename + ".mtl";
+    const char* error = 0;
+    data = parseObj(fileObjPath.c_str(), meshes, &error);
 
-    size_t lastindex2 = fileObjPath.find_last_of("/");
-    std::string rawname = fileObjPath.substr(lastindex2 + 1, lastindex - lastindex2 - 1);
+    std::cout << meshes.size() << std::endl;
+    for (const auto & mesh : meshes) {
+        std::cout << mesh.target_names.size() << std::endl;
+        for (const auto & name : mesh.target_names) {
+            std::cout << name << std::endl;
+        }
+    }
 
-//    m_rawName = rawname;
+
+
+
+//    loadMesh(m_internal, fileObjPath.c_str());
+//    process(m_internal, fileObjPath.c_str());
+
+    //    size_t lastindex              = fileObjPath.find_last_of( "." );
+    //    std::string filename          = fileObjPath.substr( 0, lastindex );
+    //    const std::string fileMtlPath = filename + ".mtl";
+
+    //    size_t lastindex2   = fileObjPath.find_last_of( "/" );
+    //    std::string rawname = fileObjPath.substr( lastindex2 + 1, lastindex - lastindex2 - 1 );
+
+    //    m_name = rawname;
     //    std::cout << "[Mesh] dirPath = " << dirPath << std::endl;
 
-    std::ifstream inFileObj;
-    inFileObj.open( fileObjPath );
-    assert(inFileObj.is_open());
-    std::stringstream strStreamObj;
-    strStreamObj << inFileObj.rdbuf();
-    const std::string fileObjTxt = strStreamObj.str();
+    //    //    std::string inputfile = "cornell_box.obj";
+    //        tinyobj::ObjReaderConfig reader_config;
+    //    //    reader_config.mtl_search_path = "./"; // Path to material files
+    //        reader_config.mtl_search_path = dirPath; // Path to material files
 
-    std::ifstream inFileMtl;
-    inFileMtl.open( fileMtlPath );
-    assert(inFileMtl.is_open());
-    std::stringstream strStreamMtl;
-    strStreamMtl << inFileMtl.rdbuf();
-    const std::string fileMtlTxt = strStreamMtl.str();
+    //    tinyobj::ObjReader reader;
 
-    std::vector<char> buff;
+    //        if ( !reader.ParseFromFile( fileObjPath, reader_config ) ) {
+    //            if ( !reader.Error().empty() ) { std::cerr << "TinyObjReader: " << reader.Error();
+    //            } exit( 1 );
+    //        }
 
-    io::Memory<decltype( buff )> memory( buff );
-    memory.write(rawname);
-    memory.write(fileObjTxt);
-    memory.write(fileMtlTxt);
+    //    if ( !reader.Warning().empty() ) { std::cout << "TinyObjReader: " << reader.Warning(); }
 
-//    assert(buff.size() == fileObjTxt.size() + fileMtlTxt.size() + 8 );
-    assert(buff.size() == fileObjTxt.size() + fileMtlTxt.size() + 8 + rawname.size() + 4);
-    m_data = new unsigned char[buff.size()];
-    m_size = buff.size();
-    memcpy(m_data, buff.data(), m_size);
+    //    m_attrib = reader.GetAttrib();
+    //    m_shapes = reader.GetShapes();
+    //    m_materials = reader.GetMaterials();
 
-    unpack();
+    //    tinyobj::attrib_t m_attrib = reader.GetAttrib();
+    //    std::vector<tinyobj::shape_t> m_shapes = reader.GetShapes();
+    //    std::vector<tinyobj::material_t> m_materials = reader.GetMaterials();
+
+    //    std::ifstream inFileObj;
+    //    inFileObj.open( fileObjPath );
+    //    assert( inFileObj.is_open() );
+    //    std::stringstream strStreamObj;
+    //    strStreamObj << inFileObj.rdbuf();
+    //    const std::string fileObjTxt = strStreamObj.str();
+
+    //    std::ifstream inFileMtl;
+    //    inFileMtl.open( fileMtlPath );
+    //    assert( inFileMtl.is_open() );
+    //    std::stringstream strStreamMtl;
+    //    strStreamMtl << inFileMtl.rdbuf();
+    //    const std::string fileMtlTxt = strStreamMtl.str();
+
+    //    std::vector<char> buff;
+
+    //    io::Memory<decltype( buff )> memory( buff );
+    //    memory.write( rawname );
+    //    memory.write( fileObjTxt );
+    //    memory.write( fileMtlTxt );
+
+    //    //    assert(buff.size() == fileObjTxt.size() + fileMtlTxt.size() + 8 );
+    //    assert( buff.size() == fileObjTxt.size() + fileMtlTxt.size() + 8 + rawname.size() + 4 );
+    //    m_data = new unsigned char[buff.size()];
+    //    m_size = buff.size();
+    //    memcpy( m_data, buff.data(), m_size );
+
+    //    unpack();
 }
 
-void Mesh::unpack()
-{
+void Mesh::unpack() {
     std::vector<char> buff;
-    buff.insert(buff.begin(), m_data, m_data + m_size);
+    buff.insert( buff.begin(), m_data, m_data + m_size );
     io::Memory<decltype( buff )> memory( buff );
 
-    memory.read(m_rawName);
+    memory.read( m_name );
     std::string fileObjTxtReaded;
-    memory.read(fileObjTxtReaded);
+    memory.read( fileObjTxtReaded );
     std::string fileMtlTxtReaded;
-    memory.read(fileMtlTxtReaded);
+    memory.read( fileMtlTxtReaded );
 
-    assert(memory.isEnd());
+    assert( memory.isEnd() );
 
     //    //    std::string inputfile = "cornell_box.obj";
     //    tinyobj::ObjReaderConfig reader_config;
     //    //    reader_config.mtl_search_path = "./"; // Path to material files
     //    reader_config.mtl_search_path = dirPath; // Path to material files
 
-    tinyobj::ObjReader reader;
+    //    tinyobj::ObjReader reader;
 
     //    if ( !reader.ParseFromFile( fileObjPath, reader_config ) ) {
     //        if ( !reader.Error().empty() ) { std::cerr << "TinyObjReader: " << reader.Error(); }
     //        exit( 1 );
     //    }
-    if ( !reader.ParseFromString( fileObjTxtReaded, fileMtlTxtReaded ) ) {
-        if ( !reader.Error().empty() ) { std::cerr << "TinyObjReader: " << reader.Error(); }
-        exit( 1 );
-    }
+    //    if ( !reader.ParseFromString( fileObjTxtReaded, fileMtlTxtReaded ) ) {
+    //        if ( !reader.Error().empty() ) { std::cerr << "TinyObjReader: " << reader.Error(); }
+    //        exit( 1 );
+    //    }
 
-    if ( !reader.Warning().empty() ) { std::cout << "TinyObjReader: " << reader.Warning(); }
+    //    if ( !reader.Warning().empty() ) { std::cout << "TinyObjReader: " << reader.Warning(); }
 
-    m_attrib = reader.GetAttrib();
-    m_shapes = reader.GetShapes();
+    //    m_attrib = reader.GetAttrib();
+    //    m_shapes = reader.GetShapes();
 
-    m_materials = reader.GetMaterials();
-
+    //    m_materials = reader.GetMaterials();
 }
 
-void Mesh::pack()
-{
+void Mesh::pack() {}
 
-}
-
-
-
-//Mesh::Mesh(const std::string &objTxt, const std::string &mtlTxt)
-//    : Measure( (unsigned char*)nullptr, 0, Resolution { { 1 }, Format::MESH } )
+// Mesh::Mesh(const std::string &objTxt, const std::string &mtlTxt)
+//     : Measure( (unsigned char*)nullptr, 0, Resolution { { 1 }, Format::MESH } )
 //{
-//    tinyobj::ObjReader reader;
+//     tinyobj::ObjReader reader;
 
 //    //    if ( !reader.ParseFromFile( fileObjPath, reader_config ) ) {
 //    //        if ( !reader.Error().empty() ) { std::cerr << "TinyObjReader: " << reader.Error(); }
@@ -157,25 +196,30 @@ void Mesh::pack()
 //    memcpy(m_data, buff.data(), m_size);
 //}
 
-std::string Mesh::to_string() const
-{
-//    std::string str = "[";
-//    return
-//    size_t nTriangle = 0;
-    size_t nVertice = m_attrib.vertices.size() / 3;
-//    for ( size_t s = 0; s < m_shapes.size(); s++ ) {
-//        const auto & shape = m_shapes.at(s);
-////        str += shape.name + ": nb points = " + std::to_string(shape.points.indices.size());
-//        const int nFace = shape.mesh.num_face_vertices.size();
-//        nTriangle += nFace;
-////        str += shape.name + ": nTriangle = " + std::to_string(nFace);
-////        if (s != m_shapes.size() - 1) {
-////            str += ", ";
-////        }
-//    }
-//    std::string str = "[nShape = " + std::to_string(m_shapes.size()) + ", nTriangle = " + std::to_string(nTriangle) + "]";
-    std::string str = "['" + m_rawName + "', nShape = " + std::to_string(m_shapes.size()) + ", nVertice = " + std::to_string(nVertice) + "]";
-//    str += "]";
+std::string Mesh::to_string() const {
+    std::string str = "";
+    str += "'" + m_name + "'";
+    str += ", nVertice:" + std::to_string(m_internal.vertices.size());
+    str += ", nTriangle:" + std::to_string(m_internal.indices.size() / 3);
+
+    //    return
+    //    size_t nTriangle = 0;
+    //    size_t nVertice = m_attrib.vertices.size() / 3;
+    //    for ( size_t s = 0; s < m_shapes.size(); s++ ) {
+    //        const auto & shape = m_shapes.at(s);
+    ////        str += shape.name + ": nb points = " + std::to_string(shape.points.indices.size());
+    //        const int nFace = shape.mesh.num_face_vertices.size();
+    //        nTriangle += nFace;
+    ////        str += shape.name + ": nTriangle = " + std::to_string(nFace);
+    ////        if (s != m_shapes.size() - 1) {
+    ////            str += ", ";
+    ////        }
+    //    }
+    //    std::string str = "[nShape = " + std::to_string(m_shapes.size()) + ", nTriangle = " +
+    //    std::to_string(nTriangle) + "]";
+    //    std::string str = "['" + m_name + "', nShape = " + std::to_string( m_shapes.size() ) +
+    //                      ", nVertice = " + std::to_string( nVertice ) + "]";
+    //    str += "]";
     return str;
 }
 
--- tests/test-data-Mesh.cpp
+++ tests/test-data-Mesh.cpp
@@ -10,90 +10,94 @@ TEST_CASE( "Mesh test" ) {
 
     const std::string meshPath = HUB_DIR "tests/data/meshes/";
 
-    hub::data::Mesh mesh( meshPath + "quad.obj" );
+    hub::data::Mesh mesh( "quad" );
+    mesh.load(meshPath + "quad.obj");
+//    hub::data::Mesh mesh( meshPath + "quad.obj" );
 
-    const auto& attrib = mesh.getAttrib();
-    const auto& shapes = mesh.getShapes();
-    assert( shapes.size() == 1 );
-    const auto& materials = mesh.getMaterials();
+    std::cout << mesh << std::endl;
 
-    CHECK( shapes.size() == 1 );
-    CHECK( shapes[0].mesh.num_face_vertices.size() == 2 );
-    CHECK( shapes[0].mesh.num_face_vertices[0] == 3 );
-    CHECK( shapes[0].mesh.num_face_vertices[1] == 3 );
+//    const auto& attrib = mesh.getAttrib();
+//    const auto& shapes = mesh.getShapes();
+//    assert( shapes.size() == 1 );
+//    const auto& materials = mesh.getMaterials();
 
-    // Loop over shapes
-    for ( size_t s = 0; s < shapes.size(); s++ ) {
-        //        std::cout << "[Mesh] shape " << s << std::endl;
-        // Loop over faces(polygon)
-        size_t index_offset = 0;
-        for ( size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++ ) {
-            //            std::cout << "[Mesh] face " << f << std::endl;
-            size_t fv = size_t( shapes[s].mesh.num_face_vertices[f] );
+//    CHECK( shapes.size() == 1 );
+//    CHECK( shapes[0].mesh.num_face_vertices.size() == 2 );
+//    CHECK( shapes[0].mesh.num_face_vertices[0] == 3 );
+//    CHECK( shapes[0].mesh.num_face_vertices[1] == 3 );
 
-            // Loop over vertices in the face.
-            for ( size_t v = 0; v < fv; v++ ) {
-                //                std::cout << "[Mesh] vertex " << v << std::endl;
-                // access to vertex
-                tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
-                tinyobj::real_t vx   = attrib.vertices[3 * size_t( idx.vertex_index ) + 0];
-                std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
-                          << " vertex vx " << vx << std::endl;
-                tinyobj::real_t vy = attrib.vertices[3 * size_t( idx.vertex_index ) + 1];
-                std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
-                          << " vertex vy " << vy << std::endl;
-                tinyobj::real_t vz = attrib.vertices[3 * size_t( idx.vertex_index ) + 2];
-                std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
-                          << " vertex vz " << vz << std::endl;
+//    // Loop over shapes
+//    for ( size_t s = 0; s < shapes.size(); s++ ) {
+//        //        std::cout << "[Mesh] shape " << s << std::endl;
+//        // Loop over faces(polygon)
+//        size_t index_offset = 0;
+//        for ( size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++ ) {
+//            //            std::cout << "[Mesh] face " << f << std::endl;
+//            size_t fv = size_t( shapes[s].mesh.num_face_vertices[f] );
 
-                // Check if `normal_index` is zero or positive. negative = no normal data
-                if ( idx.normal_index >= 0 ) {
-                    tinyobj::real_t nx = attrib.normals[3 * size_t( idx.normal_index ) + 0];
-                    std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
-                              << " vertex nx " << nx << std::endl;
-                    tinyobj::real_t ny = attrib.normals[3 * size_t( idx.normal_index ) + 1];
-                    std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
-                              << " vertex ny " << ny << std::endl;
-                    tinyobj::real_t nz = attrib.normals[3 * size_t( idx.normal_index ) + 2];
-                    std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
-                              << " vertex nz " << nz << std::endl;
-                }
+//            // Loop over vertices in the face.
+//            for ( size_t v = 0; v < fv; v++ ) {
+//                //                std::cout << "[Mesh] vertex " << v << std::endl;
+//                // access to vertex
+//                tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
+//                tinyobj::real_t vx   = attrib.vertices[3 * size_t( idx.vertex_index ) + 0];
+//                std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
+//                          << " vertex vx " << vx << std::endl;
+//                tinyobj::real_t vy = attrib.vertices[3 * size_t( idx.vertex_index ) + 1];
+//                std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
+//                          << " vertex vy " << vy << std::endl;
+//                tinyobj::real_t vz = attrib.vertices[3 * size_t( idx.vertex_index ) + 2];
+//                std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
+//                          << " vertex vz " << vz << std::endl;
 
-                // Check if `texcoord_index` is zero or positive. negative = no texcoord data
-                if ( idx.texcoord_index >= 0 ) {
-                    tinyobj::real_t tx = attrib.texcoords[2 * size_t( idx.texcoord_index ) + 0];
-                    std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
-                              << " vertex tx " << tx << std::endl;
-                    tinyobj::real_t ty = attrib.texcoords[2 * size_t( idx.texcoord_index ) + 1];
-                    std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
-                              << " vertex ty " << ty << std::endl;
-                }
+//                // Check if `normal_index` is zero or positive. negative = no normal data
+//                if ( idx.normal_index >= 0 ) {
+//                    tinyobj::real_t nx = attrib.normals[3 * size_t( idx.normal_index ) + 0];
+//                    std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
+//                              << " vertex nx " << nx << std::endl;
+//                    tinyobj::real_t ny = attrib.normals[3 * size_t( idx.normal_index ) + 1];
+//                    std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
+//                              << " vertex ny " << ny << std::endl;
+//                    tinyobj::real_t nz = attrib.normals[3 * size_t( idx.normal_index ) + 2];
+//                    std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
+//                              << " vertex nz " << nz << std::endl;
+//                }
 
-                // Optional: vertex colors
-                tinyobj::real_t red = attrib.colors[3 * size_t( idx.vertex_index ) + 0];
-                std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
-                          << " vertex color red " << red << std::endl;
-                tinyobj::real_t green = attrib.colors[3 * size_t( idx.vertex_index ) + 1];
-                std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
-                          << " vertex color green " << green << std::endl;
-                tinyobj::real_t blue = attrib.colors[3 * size_t( idx.vertex_index ) + 2];
-                std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
-                          << " vertex color blue " << blue << std::endl;
-            }
-            index_offset += fv;
+//                // Check if `texcoord_index` is zero or positive. negative = no texcoord data
+//                if ( idx.texcoord_index >= 0 ) {
+//                    tinyobj::real_t tx = attrib.texcoords[2 * size_t( idx.texcoord_index ) + 0];
+//                    std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
+//                              << " vertex tx " << tx << std::endl;
+//                    tinyobj::real_t ty = attrib.texcoords[2 * size_t( idx.texcoord_index ) + 1];
+//                    std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
+//                              << " vertex ty " << ty << std::endl;
+//                }
 
-            // per-face material
-            std::cout << "[Mesh] shape " << s << " face " << f << " material "
-                      << shapes[s].mesh.material_ids[f] << std::endl;
-        }
-    }
+//                // Optional: vertex colors
+//                tinyobj::real_t red = attrib.colors[3 * size_t( idx.vertex_index ) + 0];
+//                std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
+//                          << " vertex color red " << red << std::endl;
+//                tinyobj::real_t green = attrib.colors[3 * size_t( idx.vertex_index ) + 1];
+//                std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
+//                          << " vertex color green " << green << std::endl;
+//                tinyobj::real_t blue = attrib.colors[3 * size_t( idx.vertex_index ) + 2];
+//                std::cout << "[Mesh] shape " << s << " face " << f << " vertex " << v
+//                          << " vertex color blue " << blue << std::endl;
+//            }
+//            index_offset += fv;
 
-    for ( size_t m = 0; m < materials.size(); m++ ) {
-        //        std::cout << "[Mesh] material " << m << std::endl;
-        const auto& material = materials.at( m );
+//            // per-face material
+//            std::cout << "[Mesh] shape " << s << " face " << f << " material "
+//                      << shapes[s].mesh.material_ids[f] << std::endl;
+//        }
+//    }
 
-        std::cout << "[Mesh] material " << m << " diffuse r" << material.diffuse[0] << std::endl;
-        std::cout << "[Mesh] material " << m << " diffuse g" << material.diffuse[1] << std::endl;
-        std::cout << "[Mesh] material " << m << " diffuse b" << material.diffuse[2] << std::endl;
-    }
+//    for ( size_t m = 0; m < materials.size(); m++ ) {
+//        //        std::cout << "[Mesh] material " << m << std::endl;
+//        const auto& material = materials.at( m );
+
+//        std::cout << "[Mesh] material " << m << " diffuse r" << material.diffuse[0] << std::endl;
+//        std::cout << "[Mesh] material " << m << " diffuse g" << material.diffuse[1] << std::endl;
+//        std::cout << "[Mesh] material " << m << " diffuse b" << material.diffuse[2] << std::endl;
+//    }
 }
