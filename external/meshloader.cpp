446e0e3798e0e7bf175a1225819a2737661e782e
gauthier <gauthierbouyjou@aol.com>
2023-04-21 00:22:51 +0200

    tinyobj -> gltf
    Former-commit-id: 1c82c1f3023e9b77e6f25c09358cbf31bda5dbef [formerly 64553ee4837bf57096a54a4d298cdd94684d4781]
    Former-commit-id: ffae2adee677b0fec17b3f00e9e0ef417159ee69
    Former-commit-id: 713ea8f5c943be1e90f6aa80fd2f193a857c26c9

---
+++ external/meshloader.cpp
@@ -0,0 +1,15 @@
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#define CGLTF_IMPLEMENTATION
+//#include "../extern/cgltf.h"
+#include "cgltf.h"
+
+#define FAST_OBJ_IMPLEMENTATION
+//#include "../extern/fast_obj.h"
+#include "fast_obj.h"
+
+#define SDEFL_IMPLEMENTATION
+// #include "../extern/sdefl.h"
+#include <sdefl.h>
--- src/data/Mesh.hpp
+++ src/data/Mesh.hpp
@@ -4,32 +4,44 @@
 
 #include "Measure.hpp"
 
-//#define TINYOBJLOADER_IMPLEMENTATION // define this in only *one* .cc
-// Optional. define TINYOBJLOADER_USE_MAPBOX_EARCUT gives robust trinagulation. Requires C++11
-//#define TINYOBJLOADER_USE_MAPBOX_EARCUT
-#include "tiny_obj_loader.h"
-
+// #define TINYOBJLOADER_IMPLEMENTATION // define this in only *one* .cc
+//  Optional. define TINYOBJLOADER_USE_MAPBOX_EARCUT gives robust trinagulation. Requires C++11
+// #define TINYOBJLOADER_USE_MAPBOX_EARCUT
+// #include "tiny_obj_loader.h"
 
 namespace hub {
 namespace data {
 
-//class SRC_API Mesh : public Measure
+struct Vertex {
+    float px, py, pz;
+    float nx, ny, nz;
+    float tx, ty;
+};
+
+struct Mesh_internal {
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+};
+
+// class SRC_API Mesh : public Measure
 class SRC_API Mesh : public Measure
 {
   public:
-    explicit Mesh(const Measure & measure);
+    explicit Mesh( const Measure& measure );
 
-    explicit Mesh(const Mesh& mesh);
+    explicit Mesh( const Mesh& mesh );
 
-    explicit Mesh(const std::string & fileObjPath);
+    explicit Mesh( const std::string& name );
 
-//    Mesh(const std::string & objTxt, const std::string & mtlTxt);
+    void load( const std::string& fileObjPath );
+
+    //    Mesh(const std::string & objTxt, const std::string & mtlTxt);
 
     std::string to_string() const;
 
-    const tinyobj::attrib_t & getAttrib() const;
-    const std::vector<tinyobj::shape_t> & getShapes() const;
-    const std::vector<tinyobj::material_t> & getMaterials() const;
+    //    const tinyobj::attrib_t & getAttrib() const;
+    //    const std::vector<tinyobj::shape_t> & getShapes() const;
+    //    const std::vector<tinyobj::material_t> & getMaterials() const;
 
     SRC_API friend std::ostream& operator<<( std::ostream& os, const Mesh& mesh );
 
@@ -37,30 +49,32 @@ class SRC_API Mesh : public Measure
     void unpack();
     void pack();
 
-    std::string m_rawName;
-    tinyobj::attrib_t m_attrib;
-    std::vector<tinyobj::shape_t> m_shapes;
-    std::vector<tinyobj::material_t> m_materials;
+    std::string m_name;
+    Mesh_internal m_internal;
 
+    //    std::vector<Vertex> vertices;
+    //    std::vector<unsigned int> indices;
+    //    tinyobj::attrib_t m_attrib;
+    //    std::vector<tinyobj::shape_t> m_shapes;
+    //    std::vector<tinyobj::material_t> m_materials;
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-inline const tinyobj::attrib_t & Mesh::getAttrib() const
-{
-    return m_attrib;
-}
+// inline const tinyobj::attrib_t & Mesh::getAttrib() const
+//{
+//     return m_attrib;
+// }
 
-inline const std::vector<tinyobj::shape_t> & Mesh::getShapes() const
-{
-    return m_shapes;
-}
+// inline const std::vector<tinyobj::shape_t> & Mesh::getShapes() const
+//{
+//     return m_shapes;
+// }
 
-inline const std::vector<tinyobj::material_t> & Mesh::getMaterials() const
-{
-    return m_materials;
-}
-
+// inline const std::vector<tinyobj::material_t> & Mesh::getMaterials() const
+//{
+//     return m_materials;
+// }
 
 } // namespace data
 } // namespace hub
