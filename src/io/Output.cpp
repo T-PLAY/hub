e3f78e5f5eed221619cc52789ab06b20d1f981b8
gauthier <gauthierbouyjou@aol.com>
2023-05-08 18:08:13 +0200

    added magic number
    Former-commit-id: 27ae9bf76bc90be4ab95c793185eb6b24361addc [formerly f7d874433b4159db19866a3daceb2c9a1eaa7aee]
    Former-commit-id: e8a93a88e49d9f3bbb6df0ab6746afbf5e794260
    Former-commit-id: f3e2f3f14eeb3559e20347cf4ec8e711332bf5f1

--- src/io/Output.cpp
+++ src/io/Output.cpp
@@ -47,9 +47,9 @@ void Output::write( const SensorSpec& sensorSpec ) {
 
     char magicNumber[80] = {0};
     constexpr char joker = ' ';
-    memset(magicNumber, joker, 80);
+    memset(magicNumber, joker, 79);
     sprintf (magicNumber, "%c%c%c %d.%d.%d", 'H', 'U', 'B', hub::s_versionMajor, hub::s_versionMinor, hub::s_versionPatch);
-    assert(strlen(magicNumber) < 80);
+    assert(strlen(magicNumber) < 79);
     magicNumber[strlen(magicNumber)] = joker;
     write((unsigned char*)magicNumber, 80);
 
