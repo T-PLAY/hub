0b315fdb5a1a0cb36cd46ed25d34a9eb1e71f11c
gauthier <gauthierbouyjou@aol.com>
2023-05-18 16:59:13 +0200

    fixup cmake
    Former-commit-id: 18d95f90c8b922745f0438e252861aff64269925 [formerly c672e62c3d24e8a688d66264c999729bd6c20c38]
    Former-commit-id: cfaef6854ac289742820642b2f30d0ebd40a6951
    Former-commit-id: c3387d60f36cd3fe6d56d308bf4355d7963644f4

--- examples/example-void.cpp
+++ examples/example-void.cpp
@@ -7,7 +7,11 @@ int main() {
     char magicNumber[80] = {0};
 //    constexpr char joker = ' ';
 //    memset(magicNumber, joker, 79);
+#ifdef WIN32
     sprintf_s (magicNumber, "%c%c%c %d.%d.%d", 'H', 'U', 'B', 1, 1, 0);
+#else
+    sprintf (magicNumber, "%c%c%c %d.%d.%d", 'H', 'U', 'B', 1, 1, 0);
+#endif
 
     printf("%s\n", magicNumber);
 
@@ -20,7 +24,11 @@ int main() {
     char u;
     char b;
 //    wchar_t wc;
+#ifdef WIN32
     sscanf_s(magicNumber, "%c%c%c %d.%d.%d", &h, 1, &u, 1, &b, 1, &versionMajor, &versionMinor, &versionPatch);
+#else
+    sscanf(magicNumber, "%c%c%c %d.%d.%d", &h, 1, &u, 1, &b, 1, &versionMajor, &versionMinor, &versionPatch);
+#endif
 //    sscanf_s(magicNumber, "%c%c%c %d.%d.%d", &h, &u, &b, &versionMajor, &versionMinor, &versionPatch, (unsigned)_countof(magicNumber));
 
 
