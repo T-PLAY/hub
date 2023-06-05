02a3dbc5a1da195df25815d7e1734c619158432b
Gauthier Bouyjou <Gauthier.Bouyjou@irit.fr>
2023-06-05 17:42:11 +0200

    update script + cmake to install UWP

--- scripts/buildUWPs.sh
+++ scripts/buildUWPs.sh
@@ -6,18 +6,30 @@ if [ $(basename `pwd`) != "hub" ]; then
 	exit 0
 fi
 
-for arch in "x64"; do
-	buildDir="build-$arch"
-	mkdir -p $buildDir
-	cmake -B $buildDir . -A $arch
-	cmake --build $buildDir --config Debug
-	cmake --build $buildDir --config Release
-done
+rootDir="builds"
+mkdir -p $rootDir
+installDir="install"
 
-for arch in "x64" "ARM64"; do
-	buildDir="build-UWP-$arch"
-	mkdir -p $buildDir
-	cmake -B $buildDir . -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -A $arch
-	cmake --build $buildDir --config Debug
-	cmake --build $buildDir --config Release
+# for mode in "Debug" "Release"; do
+for mode in "Debug"; do
+	for arch in "x64"; do
+		#buildDir="build-$arch"
+		buildDir="$rootDir/$arch-$mode"
+		mkdir -p $buildDir
+		cmake -B $buildDir . -A $arch -DCMAKE_INSTALL_PREFIX=$installDir
+		cmake --build $buildDir --config $mode --parallel
+		cmake --install $buildDir --config $mode
+	done
+
+	for arch in "x64" "ARM64"; do
+			buildDir="$rootDir/UWP-$arch-$mode"
+			mkdir -p $buildDir
+			# cmake -B $buildDir . -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -A $arch
+			# cmake --build $buildDir --config Debug
+			# cmake --build $buildDir --config Release
+			cmake -B $buildDir . -A $arch -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -DCMAKE_BUILD_TYPE=$mode -DCMAKE_INSTALL_PREFIX=$installDir
+			cmake --build $buildDir --config $mode --parallel
+			cmake --install $buildDir --config $mode
+	done
+
 done
