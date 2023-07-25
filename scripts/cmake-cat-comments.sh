56a72c7ccc93b7e712ad16fb8a8dfef1180159df
Gauthier Bouyjou <Gauthier.Bouyjou@irit.fr>
2023-07-25 16:38:05 +0200

    update scripts

---
+++ scripts/cmake-cat-comments.sh
@@ -0,0 +1,19 @@
+#! /bin/bash -e
+
+rootPath=$(pwd)
+while [ $(basename "$rootPath") != "hub" ]; do
+	rootPath=$(dirname "$rootPath")
+done
+
+
+cd $rootPath
+
+for file in $(find $(cat scripts/source_dirs.txt) -type f \( -name "CMakeLists.txt" -o -name "*.cmake" \)); do
+	echo "-------------------------------- $file"
+	# cat $file | grep '^#' || sleep 0
+	# cat $file | grep '^ *#[^#].*$' || sleep 0
+	
+	cat $file | grep -n '^ *# [^A-Z^#].*$' || sleep 0
+	cat $file | grep -n '^ *#[^ ].*$' || sleep 0
+done
+
