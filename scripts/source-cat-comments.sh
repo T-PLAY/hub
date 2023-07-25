56bfcb9f5d0b3785f2b8656fa43c78fb7b4f2225
gauthier <gauthierbouyjou@aol.com>
2023-07-25 18:18:50 +0200

    update scripts
    Former-commit-id: c93070b65cf617af71fd8708ff0011e22e8f581d [formerly a552b6d6b5ca0a307a58795a844906cddcf43d8e]
    Former-commit-id: 57a0b7054fde5674d770adb31897e6793f976c7e
    Former-commit-id: 56720e584513006877fef696def7d85b88e1891c

--- scripts/source-cat-comments.sh
+++ scripts/source-cat-comments.sh
@@ -9,6 +9,8 @@ done
 cd $rootPath
 
 for file in $(find $(cat scripts/source_dirs.txt) -type f \( -name "*.cpp" -o -name "*.hpp" -o -name "*.h" -o -name "*.inl" \)); do
-	echo $file
-	cat $file | grep '^ *//[^/]' || sleep 0
+	echo "-------------------------------- $file"
+	# cat $file | grep '^ *//[^/]' || sleep 0
+	cat $file | grep '^ *// [^A-Z^a-z].*$' || sleep 0
+	# cat $file | grep '^ *//[^/^ ].*$' || sleep 0
 done
