0664e50037f61e2d240c09a19bc4d9cfd7b3e6d7
Gauthier Bouyjou <Gauthier.Bouyjou@irit.fr>
2023-07-26 14:05:32 +0200

    rename scripts

---
+++ scripts/cmake-cat-empty-comments.sh
@@ -0,0 +1,17 @@
+#! /bin/bash -e
+
+# rootPath=$(pwd)
+# while [ $(basename "$rootPath") != "hub" ]; do
+#     rootPath=$(dirname "$rootPath")
+# done
+# cd $rootPath
+
+cd $(git rev-parse --show-toplevel)
+
+for file in $(find $(cat scripts/source_dirs.txt) -type f \( -name "CMakeLists.txt" -o -name "*.cmake" \)); do
+	echo "-------------------------------- $file"
+	
+	cat $file | grep -n '^ *#[^ ].*$' || sleep 0
+	cat $file | grep -n '^ *# [^A-Z^#].*$' || sleep 0
+done
+
---
+++ scripts/source-cat-empty-comments.sh
@@ -0,0 +1,17 @@
+#! /bin/bash -e
+
+# rootPath=$(pwd)
+# while [ $(basename "$rootPath") != "hub" ]; do
+#     rootPath=$(dirname "$rootPath")
+# done
+#
+# cd $rootPath
+
+cd $(git rev-parse --show-toplevel)
+
+for file in $(find $(cat scripts/source_dirs.txt) -type f \( -name "*.cpp" -o -name "*.hpp" -o -name "*.h" -o -name "*.inl" \)); do
+	echo "-------------------------------- $file"
+
+	cat $file | grep '^ *//[^ ^/].*$' || sleep 0
+	cat $file | grep '^ *// [ ].*$' || sleep 0
+done
