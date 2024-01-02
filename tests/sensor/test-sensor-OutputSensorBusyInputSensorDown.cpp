4ffc34e74c717f748dadb0a398c19947974a1d4b
Gauthier Bouyjou <gauthierbouyjou@aol.com>
2024-01-02 16:20:36 +0100

    fix compilation on linux home

--- tests/sensor/test-sensor-OutputSensorBusyInputSensorDown.cpp
+++ tests/sensor/test-sensor-OutputSensorBusyInputSensorDown.cpp
@@ -14,6 +14,9 @@ TEST_CASE( "OutputSensor sending acq with disconnect test" ) {
 
     INIT_SERVER
 
+    constexpr auto nAcq     = 100;
+    // std::atomic<bool> begin = false;
+
     {
         hub::MetaData metaData;
         metaData["parent"] = "parentName";
@@ -40,19 +43,23 @@ TEST_CASE( "OutputSensor sending acq with disconnect test" ) {
         // auto& intRef  = acq.get<int&>();
         std::atomic<bool> exit = false;
         auto outputThread      = std::thread( [&]() {
+            // while ( !begin ) {
+            //     std::this_thread::sleep_for( std::chrono::milliseconds( 10 ) );
+            // };
             int iAcq = 0;
             while ( !exit ) {
                 start  = iAcq;
                 end    = iAcq;
-                bgr8.b = iAcq;
-                bgr8.g = iAcq;
-                bgr8.r = iAcq;
+                bgr8.b = iAcq % 256;
+                bgr8.g = iAcq % 256;
+                bgr8.r = iAcq % 256;
+                // std::cout << "write acq " << acq << std::endl;
                 outputSensor << acq;
                 ++iAcq;
                 std::cout << "\033[31m-\033[0m";
-                if ( iAcq >= 100 ) {
-                    std::this_thread::sleep_for( std::chrono::milliseconds( 1 ) );
-                }
+                // if ( iAcq >= 100 ) {
+                std::this_thread::sleep_for( std::chrono::microseconds( 100 ) );
+                // }
             }
             std::cout << std::endl;
         } );
@@ -71,22 +78,27 @@ TEST_CASE( "OutputSensor sending acq with disconnect test" ) {
             auto& start_read = acq_read.start();
             auto& end_read   = acq_read.end();
             auto& bgr8_read  = acq_read.get<hub::sensor::format::BGR8&>();
-            for ( int i = 0; i < 100; ++i ) {
+            // while ( !begin ) {
+            //     std::this_thread::sleep_for( std::chrono::milliseconds( 10 ) );
+            // };
+            // begin = true;
+            for ( int i = 0; i < nAcq; ++i ) {
                 inputSensor >> acq_read;
                 // std::cout << "read acq " << acq_read << std::endl;
                 std::cout << "\033[32m+\033[0m";
-                const int value = start_read;
+                const auto start = start_read;
                 // CHECK( start_read == i );
-                CHECK( end_read == value );
-                CHECK( bgr8_read.b == value );
-                CHECK( bgr8_read.g == value );
-                CHECK( bgr8_read.r == value );
-                    // std::this_thread::sleep_for( std::chrono::milliseconds( 10 ) );
+                CHECK( end_read == start );
+                CHECK( bgr8_read.b == start % 256 );
+                CHECK( bgr8_read.g == start % 256 );
+                CHECK( bgr8_read.r == start % 256 );
+                // std::this_thread::sleep_for( std::chrono::milliseconds( 10 ) );
             }
             std::cout << std::endl;
             std::cout << "[test] end inputSensor ------------------------------" << std::endl;
         } // end inputSensor
-        // std::this_thread::sleep_for( std::chrono::milliseconds( 1000 ) );
+
+        std::this_thread::sleep_for( std::chrono::milliseconds( 100 ) );
 
         exit = true;
         outputThread.join();
