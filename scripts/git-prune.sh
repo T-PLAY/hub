c579dc51051accc4c7456a88603fe444c8aeaf6d
Gauthier Bouyjou <bouyjou@toulouse-tech-transfer.com>
2024-03-09 11:03:23 +0100

    update script

---
+++ scripts/git-prune.sh
@@ -0,0 +1,9 @@
+#! /bin/bash -e
+
+cd $(git rev-parse --show-toplevel)
+
+# bfg --strip-blobs-bigger-than 1M .
+
+# echo "---------------------------------------------------"
+
+git reflog expire --expire=now --all && git gc --prune=now --aggressive
