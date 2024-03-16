9095833229a91f6e484c8b06040beede9797e412
Gauthier Bouyjou <bouyjou@toulouse-tech-transfer.com>
2024-03-16 19:35:51 +0100

    [ci] update cmake scripts

--- scripts/cmake-remove-empty-comments.sh
+++ scripts/cmake-remove-empty-comments.sh
@@ -8,10 +8,25 @@
 
 cd $(git rev-parse --show-toplevel)
 
+# echo -n '#
+# #should be deleted
+# # should be deleted
+# #  should be deleted
+#  #
+#  #should be deleted
+#  # should be deleted
+#     #
+#     #should be deleted
+#     # should be deleted
+# ' >> "scripts/test/CMakeLists.txt"
+
+scripts/cmake-format-all.sh
+
 for file in $(find $(cat scripts/source_dirs.txt | grep -v '^*') -type f \( -name "CMakeLists.txt" -o -name "*.cmake" \)); do
 	echo "$file"
 
+	sed -i '/^[ \t]*# *$/d' $file
 	# sed -i '/^ *#[^ ].*$/d' $file
 	# sed -i '/^ *# [^A-Z^#].*$/d' $file
-	sed -i '/^ *# [^A-Z^#^c].*$/d' $file
+	sed -i '/^[ \t]*# [^A-Z^#^c].*$/d' $file
 done
