e227ce761117fb67e05a552d9c5fa401fb09dfd2
Gauthier Bouyjou <bouyjou@toulouse-tech-transfer.com>
2024-03-18 21:42:43 +0100

    [ci] update workflow

--- scripts/cmake-format-all.sh
+++ scripts/cmake-format-all.sh
@@ -8,7 +8,14 @@
 
 cd $(git rev-parse --show-toplevel)
 
+CI_CMAKE_FORMAT_VERSION="0.6.13"
+
 # echo "using cmake-format : $(cmake-format --version)"
+CMAKE_FORMAT_VERSION=$(cmake-format --version)
+if [ "$CMAKE_FORMAT_VERSION" != "$CI_CMAKE_FORMAT_VERSION" ]; then
+	echo "cmake-format need to be compatible with CI version : $CI_CMAKE_FORMAT_VERSION"
+	exit 1
+fi
 
 for file in $(find $(cat scripts/source_dirs.txt | grep -v '^*' ) -type f \( -name "CMakeLists.txt" -o -name "*.cmake" \)); do
 	echo "$file"
