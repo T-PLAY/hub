b2e83548c772424f13f5acc36331092e8a8fa017
Gauthier Bouyjou <bouyjou@toulouse-tech-transfer.com>
2024-03-19 19:36:47 +0100

    [format] update scripts cmake and source

--- scripts/source-format-all.sh
+++ scripts/source-format-all.sh
@@ -5,10 +5,23 @@
 #     rootPath=$(dirname "$rootPath")
 # done
 # cd $rootPath
+CLANG_FORMAT_EXE=clang-format
+
+if [ -e "/opt/clang-format-static/clang-format-14" ]; then
+	CLANG_FORMAT_EXE="/opt/clang-format-static/clang-format-14"
+fi
+
+CI_CLANG_FORMAT_VERSION="14.0.0"
+
+CLANG_FORMAT_VERSION=$($CLANG_FORMAT_EXE --version | awk -F' ' '{print $3'})
+if [ "$CLANG_FORMAT_VERSION" != "$CI_CLANG_FORMAT_VERSION" ]; then
+	echo "clang-format need to be compatible with CI version : $CI_CLANG_FORMAT_VERSION"
+	exit 1
+fi
 
 cd $(git rev-parse --show-toplevel)
 
-export PATH="/opt/clang-format-static:$PATH"
+# export PATH="/opt/clang-format-static:$PATH"
 
 # echo "using clang-format : $(clang-format --version)"
 
@@ -18,9 +31,10 @@ for file in $(find $(cat scripts/source_dirs.txt | grep -v '^*') -type f \( -nam
 	# clang-format-11 -i --style=file $file
 
 	# clang-format on ubuntu ci is on version 14.0.0
-	if [ -e "/opt/clang-format-static/clang-format-14" ]; then
-		clang-format-14 -i --style=file $file
-	else
-		clang-format -i --style=file $file
-	fi
+	# if [ -e "/opt/clang-format-static/clang-format-14" ]; then
+		# clang-format-14 -i --style=file $file
+	# else
+		# clang-format -i --style=file $file
+	# fi
+	$CLANG_FORMAT_EXE -i --style=file $file
 done
