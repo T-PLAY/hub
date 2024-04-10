cc0284985ff5e5fae869e2764adf2aeb58c327a7
Gauthier Bouyjou <bouyjou@toulouse-tech-transfer.com>
2024-04-10 11:01:05 +0200

    licensed all headers

--- src/io/Header.hpp
+++ src/io/Header.hpp
@@ -1,6 +1,17 @@
-/// © 2021-2024 Hub, All Rights Reserved
-/// @author gauthier <gauthierbouyjou@aol.com>
-/// @date 2023/12/15
+/*******************************************************************************
+* Copyright (c) 2021 IRIT, computer science research laboratory, Toulouse, France.
+* Please visit https://www.irit.fr/tplay/.
+*
+* All rights reserved.
+* This code belongs to tplay/hub project (https://github.com/T-PLAY/hub).
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*
+* Initial Contributors:
+*   - Gauthier Bouyjou
+*******************************************************************************/
 
 #pragma once
 
--- src/server/Server.hpp
+++ src/server/Server.hpp
@@ -1,6 +1,18 @@
-/// © 2021-2024 Hub, All Rights Reserved
-/// @author gauthier <gauthierbouyjou@aol.com>
-/// @date 2023/03/16
+/*******************************************************************************
+* Copyright (c) 2021 IRIT, computer science research laboratory, Toulouse, France.
+* Please visit https://www.irit.fr/tplay/.
+*
+* All rights reserved.
+* This code belongs to tplay/hub project (https://github.com/T-PLAY/hub).
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*
+* Initial Contributors:
+*   - Nicolas Mellado (IRIT)
+*   - Gauthier Bouyjou (IRIT)
+*******************************************************************************/
 
 #pragma once
 
